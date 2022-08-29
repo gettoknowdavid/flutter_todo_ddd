ac28cacadd68f3c905cd3d003dbaac924bc7fa0e
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 21:43:36 +0100

    add login form and action buttons to login page

--- lib/modules/auth/presentation/pages/login_page.dart
+++ lib/modules/auth/presentation/pages/login_page.dart
@@ -1,16 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/login_form.dart';
+import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('login Page Title'),
-      ),
-      body: const Center(
-        child: Text('login Page Body'),
+      extendBodyBehindAppBar: true,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: SizeUtil.pSymmetric(h: 18, v: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizeUtil.vS(70),
+              Text(
+                'Welcome back',
+                style: TextStyle(
+                  fontSize: SizeUtil.fontSize(26),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizeUtil.vS(6),
+              Text(
+                'You can login into your account with your email and password.',
+                style: TextStyle(color: theme.primaryColorDark),
+              ),
+              SizeUtil.vS(36),
+              const LoginForm(),
+              SizeUtil.vS(16),
+              Text(
+                'or',
+                style: TextStyle(color: theme.primaryColorDark),
+                textAlign: TextAlign.center,
+              ),
+              SizeUtil.vS(16),
+              OutlinedButton.icon(
+                icon: Image.asset('assets/icons/google.png'),
+                label: const Text('Login with Google'),
+                onPressed: () {},
+              ),
+              SizeUtil.vS(16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Don\'t have an account?'),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: SizeUtil.pSymmetric(h: 6),
+                    ),
+                    child: Text(
+                      'Register now',
+                      style: TextStyle(color: theme.colorScheme.secondary),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
