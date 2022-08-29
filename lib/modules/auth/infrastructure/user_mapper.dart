273a78b2d6d5124e758fe415794f350ecc70cdbf
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 00:33:42 +0100

    Create user_mapper.dart

---
+++ lib/modules/auth/infrastructure/user_mapper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_todo_ddd/core/uid.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/entities/user.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/value_objects.dart';
+import 'package:flutter_todo_ddd/modules/auth/infrastructure/dtos/user_dto.dart';
+
+class UserMapper {
+  User? toDomain(UserDto? dto) {
+    if (dto == null) {
+      return null;
+    }
+
+    return User(
+      uid: Uid.fromUniqueString(dto.uid),
+      name: IName(dto.name),
+      email: IEmail(dto.email),
+      avatar: dto.avatar,
+    );
+  }
+}
