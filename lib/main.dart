9bb99c89584bbd3ebb431e8b1bf0ad4eec60d450
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 23:25:57 +0100

    config: initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_todo_ddd/firebase_options.dart';
 
 import 'app/app_widget.dart';
 import 'app/app_module.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await ScreenUtil.ensureScreenSize();
+
   runApp(
     ModularApp(
       module: AppModule(),
