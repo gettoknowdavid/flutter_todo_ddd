a7feef3ab11e66ea4bc40ba87963d916724466d5
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 00:55:40 +0100

    add di to app module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,14 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/pages/splash_page.dart';
-import 'package:flutter_todo_ddd/modules/auth/auth_module.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
+import 'package:flutter_todo_ddd/modules/auth/infrastructure/auth_facade.dart';
+import 'package:flutter_todo_ddd/modules/auth/infrastructure/user_mapper.dart';
+import 'package:flutter_todo_ddd/modules/auth/presentation/pages/login_page.dart';
+import 'package:flutter_todo_ddd/modules/auth/presentation/pages/register_page.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind.lazySingleton<FirebaseAuth>((i) => FirebaseAuth.instance),
+    Bind.lazySingleton<UserMapper>((i) => UserMapper()),
+    Bind.lazySingleton<IAuthFacade>((i) => AuthFacade(i(), i())),
+  ];
 
   @override
   final List<ModularRoute> routes = [
     ChildRoute(Modular.initialRoute, child: (_, __) => const SplashPage()),
-    ModuleRoute('/auth', module: AuthModule()),
+    ChildRoute('/login', child: (_, __) => const LoginPage()),
+    ChildRoute('/register', child: (_, __) => const RegisterPage()),
   ];
 }
