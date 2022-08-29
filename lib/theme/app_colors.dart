07b4fe6864e2d537bc63724b872cbdd430a76073
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 12:38:40 +0100

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF202020);
+  static const Color grey = Color(0xFFF4F4F6);
+  static const Color greyDark = Color(0xFFA5A5A6);
+  static const Color lavender = Color(0xFFE9E8EE);
+  static const Color red = Color(0xFFC42B1C);
+  static const Color orange = Color(0xFFFF854C);
+}
