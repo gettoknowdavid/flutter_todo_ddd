ae94c8303f0c6539b30982688d58e8753382b28b
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 00:56:04 +0100

    Update user_mapper.dart

--- lib/modules/auth/infrastructure/user_mapper.dart
+++ lib/modules/auth/infrastructure/user_mapper.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_todo_ddd/core/uid.dart';
 import 'package:flutter_todo_ddd/modules/auth/domain/entities/user.dart';
 import 'package:flutter_todo_ddd/modules/auth/domain/value_objects.dart';
 import 'package:flutter_todo_ddd/modules/auth/infrastructure/dtos/user_dto.dart';
 
+@Injectable()
 class UserMapper {
   User? toDomain(UserDto? dto) {
     if (dto == null) {
