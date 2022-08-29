ca56b73ffb6096d52677872ff7a01927581eae72
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 00:20:25 +0100

    create IAuthFacade with register method

---
+++ lib/modules/auth/domain/i_auth_facade.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/errors/auth_failure.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> register({
+    required IName name,
+    required IEmail email,
+    required IPassword password,
+  });
+}
