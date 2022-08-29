34897b401bab4a49658dfcef4d528938fff9ee97
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 04:43:32 +0100

    refactor: app_module

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_widget.dart';
-import 'modules/app/app_module.dart';
+import 'app/app_module.dart';
 
 void main() {
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
