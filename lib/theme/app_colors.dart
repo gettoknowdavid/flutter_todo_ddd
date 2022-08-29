870bf6e667e2b6aabc40967f6edc103ba56a6374
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 12:53:48 +0100

    fix: app color grey dark

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -6,7 +6,7 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF202020);
   static const Color grey = Color(0xFFF4F4F6);
-  static const Color greyDark = Color(0xFFA5A5A6);
+  static const Color greyDark = Color(0xFF595959);
   static const Color lavender = Color(0xFFE9E8EE);
   static const Color red = Color(0xFFC42B1C);
   static const Color orange = Color(0xFFFF854C);
