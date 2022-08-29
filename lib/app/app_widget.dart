db9cbd86cef729b623d15147f9f32600bfa51b99
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 12:44:45 +0100

    feat: add screen util init to app

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_todo_ddd/modules/app/presentation/pages/splash_page.dart';
+import 'package:flutter_todo_ddd/theme/app_theme.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'My Smart App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
+    return ScreenUtilInit(
+      designSize: const Size(393, 830),
+      minTextAdapt: true,
+      child: const SplashPage(),
+      builder: (context, child) => MaterialApp.router(
+        title: 'Teek',
+        theme: AppTheme.theme,
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
+      ),
     );
   }
 }
