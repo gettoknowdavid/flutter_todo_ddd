399379a884f4fc05ea1e63ddfa103cb4968ed7f1
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 11:30:53 +0100

    fix: linting

--- lib/modules/auth/auth_module.dart
+++ lib/modules/auth/auth_module.dart
@@ -1,14 +1,12 @@
-
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_todo_ddd/modules/auth/presentation/pages/login_page.dart';
 
 class AuthModule extends Module {
   @override
-  final List<Bind> binds = [
-
-  ];
+  final List<Bind> binds = [];
 
   @override
   final List<ModularRoute> routes = [
+    ChildRoute('/login', child: (_, __) => const LoginPage()),
   ];
 }
-    
