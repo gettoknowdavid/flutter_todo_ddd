2153e5fc24ed356bbc779ac653c85feaf8b6fd63
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 11:30:44 +0100

    Update splash_page.dart

--- lib/modules/app/presentation/pages/splash_page.dart
+++ lib/modules/app/presentation/pages/splash_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5), () {
+      Modular.to.navigate('/auth/login');
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: Text('Splash Page'),
+      ),
+    );
   }
 }
