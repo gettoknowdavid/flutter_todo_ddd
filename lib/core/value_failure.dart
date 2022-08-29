15c71db74efeeae5bca7aae452280426e97e7564
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 00:16:01 +0100

    Create value_failure.dart

---
+++ lib/core/value_failure.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'value_failure.freezed.dart';
+
+@freezed
+class ValueFailure<T> with _$ValueFailure<T> {
+  const factory ValueFailure.empty([T? f]) = Empty<T>;
+  const factory ValueFailure.invalidEmail(T f) = InvalidEmail<T>;
+  const factory ValueFailure.invalidPassword(T f) = InvalidPassword<T>;
+}
