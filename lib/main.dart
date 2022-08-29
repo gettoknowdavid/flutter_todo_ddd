2bba62ef450ce630652e1539b22e77d6cebff394
David Michael II <knowdavidmichael@gmail.com>
2022-08-29 11:30:24 +0100

    fix: linting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -6,5 +5,10 @@ import 'app/app_widget.dart';
 import 'app/app_module.dart';
 
 void main() {
-  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
 }
