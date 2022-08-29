ab05f75e671106919d8da676e9183ad49e2929f4
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 00:30:14 +0100

    Create user_dto.freezed.dart

---
+++ lib/modules/auth/infrastructure/dtos/user_dto.freezed.dart
@@ -0,0 +1,202 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'user_dto.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+UserDto _$UserDtoFromJson(Map<String, dynamic> json) {
+  return _UserDto.fromJson(json);
+}
+
+/// @nodoc
+mixin _$UserDto {
+  String get uid => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  String? get avatar => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $UserDtoCopyWith<UserDto> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserDtoCopyWith<$Res> {
+  factory $UserDtoCopyWith(UserDto value, $Res Function(UserDto) then) =
+      _$UserDtoCopyWithImpl<$Res>;
+  $Res call({String uid, String name, String email, String? avatar});
+}
+
+/// @nodoc
+class _$UserDtoCopyWithImpl<$Res> implements $UserDtoCopyWith<$Res> {
+  _$UserDtoCopyWithImpl(this._value, this._then);
+
+  final UserDto _value;
+  // ignore: unused_field
+  final $Res Function(UserDto) _then;
+
+  @override
+  $Res call({
+    Object? uid = freezed,
+    Object? name = freezed,
+    Object? email = freezed,
+    Object? avatar = freezed,
+  }) {
+    return _then(_value.copyWith(
+      uid: uid == freezed
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      avatar: avatar == freezed
+          ? _value.avatar
+          : avatar // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_UserDtoCopyWith<$Res> implements $UserDtoCopyWith<$Res> {
+  factory _$$_UserDtoCopyWith(
+          _$_UserDto value, $Res Function(_$_UserDto) then) =
+      __$$_UserDtoCopyWithImpl<$Res>;
+  @override
+  $Res call({String uid, String name, String email, String? avatar});
+}
+
+/// @nodoc
+class __$$_UserDtoCopyWithImpl<$Res> extends _$UserDtoCopyWithImpl<$Res>
+    implements _$$_UserDtoCopyWith<$Res> {
+  __$$_UserDtoCopyWithImpl(_$_UserDto _value, $Res Function(_$_UserDto) _then)
+      : super(_value, (v) => _then(v as _$_UserDto));
+
+  @override
+  _$_UserDto get _value => super._value as _$_UserDto;
+
+  @override
+  $Res call({
+    Object? uid = freezed,
+    Object? name = freezed,
+    Object? email = freezed,
+    Object? avatar = freezed,
+  }) {
+    return _then(_$_UserDto(
+      uid: uid == freezed
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      avatar: avatar == freezed
+          ? _value.avatar
+          : avatar // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_UserDto implements _UserDto {
+  _$_UserDto(
+      {required this.uid,
+      required this.name,
+      required this.email,
+      this.avatar});
+
+  factory _$_UserDto.fromJson(Map<String, dynamic> json) =>
+      _$$_UserDtoFromJson(json);
+
+  @override
+  final String uid;
+  @override
+  final String name;
+  @override
+  final String email;
+  @override
+  final String? avatar;
+
+  @override
+  String toString() {
+    return 'UserDto(uid: $uid, name: $name, email: $email, avatar: $avatar)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_UserDto &&
+            const DeepCollectionEquality().equals(other.uid, uid) &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.email, email) &&
+            const DeepCollectionEquality().equals(other.avatar, avatar));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(uid),
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(email),
+      const DeepCollectionEquality().hash(avatar));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_UserDtoCopyWith<_$_UserDto> get copyWith =>
+      __$$_UserDtoCopyWithImpl<_$_UserDto>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_UserDtoToJson(
+      this,
+    );
+  }
+}
+
+abstract class _UserDto implements UserDto {
+  factory _UserDto(
+      {required final String uid,
+      required final String name,
+      required final String email,
+      final String? avatar}) = _$_UserDto;
+
+  factory _UserDto.fromJson(Map<String, dynamic> json) = _$_UserDto.fromJson;
+
+  @override
+  String get uid;
+  @override
+  String get name;
+  @override
+  String get email;
+  @override
+  String? get avatar;
+  @override
+  @JsonKey(ignore: true)
+  _$$_UserDtoCopyWith<_$_UserDto> get copyWith =>
+      throw _privateConstructorUsedError;
+}
