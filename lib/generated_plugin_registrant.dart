64acf6683b8ab09244a7fb72cbb880b744aee7e7
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 20:53:05 +0100

    chore: add google_sign_in dep

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,6 +9,7 @@
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -18,6 +19,7 @@ void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
