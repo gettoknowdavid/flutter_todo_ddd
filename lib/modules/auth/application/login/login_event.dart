96073ffa785e6c97727159c1df43960a5835eae1
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 01:25:36 +0100

    Create login_event.dart

---
+++ lib/modules/auth/application/login/login_event.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'login_event.freezed.dart';
+
+@freezed
+class LoginEvent with _$LoginEvent {
+  const factory LoginEvent.emailChanged(String email) = EmailChanged;
+
+  const factory LoginEvent.passwordChanged(String password) = PasswordChanged;
+
+  const factory LoginEvent.signInPressed() = SignInPressed;
+}
