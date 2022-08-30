413081e077f297f12d4935cf361d65f890988f16
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 02:11:25 +0100

    update di

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/pages/splash_page.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_controller.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/login/login_controller.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/register/register_controller.dart';
 import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
 import 'package:flutter_todo_ddd/modules/auth/infrastructure/auth_facade.dart';
 import 'package:flutter_todo_ddd/modules/auth/infrastructure/user_mapper.dart';
@@ -13,6 +16,9 @@ class AppModule extends Module {
     Bind.lazySingleton<FirebaseAuth>((i) => FirebaseAuth.instance),
     Bind.lazySingleton<UserMapper>((i) => UserMapper()),
     Bind.lazySingleton<IAuthFacade>((i) => AuthFacade(i(), i())),
+    Bind.lazySingleton<LoginController>((i) => LoginController(i())),
+    Bind.lazySingleton<RegisterController>((i) => RegisterController(i())),
+    Bind.lazySingleton<AuthController>((i) => AuthController(i())),
   ];
 
   @override
