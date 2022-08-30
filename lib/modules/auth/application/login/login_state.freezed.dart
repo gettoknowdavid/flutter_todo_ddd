66e5f6b923f871a66e8ebc0a3dab9aa73b6e3e11
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 01:25:40 +0100

    Create login_state.freezed.dart

---
+++ lib/modules/auth/application/login/login_state.freezed.dart
@@ -0,0 +1,220 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'login_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$LoginState {
+  IEmail get email => throw _privateConstructorUsedError;
+  IPassword get password => throw _privateConstructorUsedError;
+  bool get loading => throw _privateConstructorUsedError;
+  bool get showError => throw _privateConstructorUsedError;
+  Option<Either<AuthFailure, Unit>> get authOption =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $LoginStateCopyWith<LoginState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LoginStateCopyWith<$Res> {
+  factory $LoginStateCopyWith(
+          LoginState value, $Res Function(LoginState) then) =
+      _$LoginStateCopyWithImpl<$Res>;
+  $Res call(
+      {IEmail email,
+      IPassword password,
+      bool loading,
+      bool showError,
+      Option<Either<AuthFailure, Unit>> authOption});
+}
+
+/// @nodoc
+class _$LoginStateCopyWithImpl<$Res> implements $LoginStateCopyWith<$Res> {
+  _$LoginStateCopyWithImpl(this._value, this._then);
+
+  final LoginState _value;
+  // ignore: unused_field
+  final $Res Function(LoginState) _then;
+
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? loading = freezed,
+    Object? showError = freezed,
+    Object? authOption = freezed,
+  }) {
+    return _then(_value.copyWith(
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as IEmail,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as IPassword,
+      loading: loading == freezed
+          ? _value.loading
+          : loading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      showError: showError == freezed
+          ? _value.showError
+          : showError // ignore: cast_nullable_to_non_nullable
+              as bool,
+      authOption: authOption == freezed
+          ? _value.authOption
+          : authOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<AuthFailure, Unit>>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_LoginStateCopyWith<$Res>
+    implements $LoginStateCopyWith<$Res> {
+  factory _$$_LoginStateCopyWith(
+          _$_LoginState value, $Res Function(_$_LoginState) then) =
+      __$$_LoginStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {IEmail email,
+      IPassword password,
+      bool loading,
+      bool showError,
+      Option<Either<AuthFailure, Unit>> authOption});
+}
+
+/// @nodoc
+class __$$_LoginStateCopyWithImpl<$Res> extends _$LoginStateCopyWithImpl<$Res>
+    implements _$$_LoginStateCopyWith<$Res> {
+  __$$_LoginStateCopyWithImpl(
+      _$_LoginState _value, $Res Function(_$_LoginState) _then)
+      : super(_value, (v) => _then(v as _$_LoginState));
+
+  @override
+  _$_LoginState get _value => super._value as _$_LoginState;
+
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? loading = freezed,
+    Object? showError = freezed,
+    Object? authOption = freezed,
+  }) {
+    return _then(_$_LoginState(
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as IEmail,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as IPassword,
+      loading: loading == freezed
+          ? _value.loading
+          : loading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      showError: showError == freezed
+          ? _value.showError
+          : showError // ignore: cast_nullable_to_non_nullable
+              as bool,
+      authOption: authOption == freezed
+          ? _value.authOption
+          : authOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<AuthFailure, Unit>>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_LoginState implements _LoginState {
+  const _$_LoginState(
+      {required this.email,
+      required this.password,
+      required this.loading,
+      required this.showError,
+      required this.authOption});
+
+  @override
+  final IEmail email;
+  @override
+  final IPassword password;
+  @override
+  final bool loading;
+  @override
+  final bool showError;
+  @override
+  final Option<Either<AuthFailure, Unit>> authOption;
+
+  @override
+  String toString() {
+    return 'LoginState(email: $email, password: $password, loading: $loading, showError: $showError, authOption: $authOption)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_LoginState &&
+            const DeepCollectionEquality().equals(other.email, email) &&
+            const DeepCollectionEquality().equals(other.password, password) &&
+            const DeepCollectionEquality().equals(other.loading, loading) &&
+            const DeepCollectionEquality().equals(other.showError, showError) &&
+            const DeepCollectionEquality()
+                .equals(other.authOption, authOption));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(email),
+      const DeepCollectionEquality().hash(password),
+      const DeepCollectionEquality().hash(loading),
+      const DeepCollectionEquality().hash(showError),
+      const DeepCollectionEquality().hash(authOption));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_LoginStateCopyWith<_$_LoginState> get copyWith =>
+      __$$_LoginStateCopyWithImpl<_$_LoginState>(this, _$identity);
+}
+
+abstract class _LoginState implements LoginState {
+  const factory _LoginState(
+          {required final IEmail email,
+          required final IPassword password,
+          required final bool loading,
+          required final bool showError,
+          required final Option<Either<AuthFailure, Unit>> authOption}) =
+      _$_LoginState;
+
+  @override
+  IEmail get email;
+  @override
+  IPassword get password;
+  @override
+  bool get loading;
+  @override
+  bool get showError;
+  @override
+  Option<Either<AuthFailure, Unit>> get authOption;
+  @override
+  @JsonKey(ignore: true)
+  _$$_LoginStateCopyWith<_$_LoginState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
