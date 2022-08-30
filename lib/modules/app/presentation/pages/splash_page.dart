6ddaf527d4c66985adcb381817d6a18f80992896
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 20:37:26 +0100

    refactor: authProvider in splash page

--- lib/modules/app/presentation/pages/splash_page.dart
+++ lib/modules/app/presentation/pages/splash_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/auth_controller.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
-import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
-
-final _authProvider = StateNotifierProvider<AuthController, AuthState>(
-  (ref) => AuthController(Modular.get<IAuthFacade>()),
-);
 
 class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -20,7 +15,7 @@ class SplashPage extends ConsumerStatefulWidget {
 class _SplashPageState extends ConsumerState<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    ref.listen<AuthState>(_authProvider, (previous, next) {
+    ref.listen<AuthState>(authProvider, (previous, next) {
       Future.delayed(const Duration(seconds: 5), () {
         next.mapOrNull(
           authenticated: (_) {
@@ -42,7 +37,7 @@ class _SplashPageState extends ConsumerState<SplashPage> {
 
   @override
   void initState() {
-    final authEvent = ref.read(_authProvider.notifier);
+    final authEvent = ref.read(authProvider.notifier);
     authEvent.mapEventsToStates(const AuthEvent.checkRequested());
     authEvent.mapEventsToStates(const AuthEvent.checkVerified());
     super.initState();
