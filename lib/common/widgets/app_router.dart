2999a15bde04f4cd278dc2e6aa016255824376f7
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 03:26:10 +0100

    Create app_router.dart

---
+++ lib/common/widgets/app_router.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  AppRouter._();
+
+  static final key = GlobalKey<NavigatorState>();
+  static final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+}
