c6cff0c9d75ee128016a6701fe62fda708916b6a
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 03:26:05 +0100

    config: set navigator key

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_router.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/pages/splash_page.dart';
 import 'package:flutter_todo_ddd/theme/app_theme.dart';
 
@@ -9,6 +10,8 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Modular.setNavigatorKey(AppRouter.key);
+
     return ScreenUtilInit(
       designSize: const Size(393, 830),
       minTextAdapt: true,
@@ -16,6 +19,7 @@ class AppWidget extends StatelessWidget {
       builder: (context, child) => MaterialApp.router(
         title: 'Teek',
         theme: AppTheme.theme,
+        scaffoldMessengerKey: AppRouter.scaffoldMessengerKey,
         routeInformationParser: Modular.routeInformationParser,
         routerDelegate: Modular.routerDelegate,
       ),
