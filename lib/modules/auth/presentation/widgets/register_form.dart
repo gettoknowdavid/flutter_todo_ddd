55e5b83684a29f61bc0f865a484a6a5508384631
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 18:02:18 +0100

    refactor: authProvider and registerProvider

--- lib/modules/auth/presentation/widgets/register_form.dart
+++ lib/modules/auth/presentation/widgets/register_form.dart
@@ -2,26 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/common/widgets/app_button.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_snackbars.dart';
 import 'package:flutter_todo_ddd/common/widgets/app_text_field.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/auth_controller.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/register/register_controller.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/register/register_event.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/register/register_state.dart';
-import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
 import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/password_rules_widget.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
-
-final authProvider = StateNotifierProvider<AuthController, AuthState>(
-  (ref) => AuthController(Modular.get<IAuthFacade>()),
-);
-
-final registerProvider =
-    StateNotifierProvider.autoDispose<RegisterController, RegisterState>(
-  (ref) {
-    return RegisterController(Modular.get<IAuthFacade>());
-  },
-);
 
 class RegisterForm extends ConsumerWidget {
   final formKey = GlobalKey<FormState>();
@@ -33,6 +21,30 @@ class RegisterForm extends ConsumerWidget {
     final state = ref.watch(registerProvider);
     final event = ref.watch(registerProvider.notifier);
     final authEvent = ref.watch(authProvider.notifier);
+
+    ref.listen<RegisterState>(registerProvider, (p, c) {
+      c.authOption.fold(
+        () => null,
+        (either) => either.fold(
+          (failure) {
+            AppSnackbar.errorSnackbar(
+              title: 'Registration Error',
+              message: failure.maybeMap(
+                emailInUse: (_) => 'This email has already been used.',
+                orElse: () => '',
+                serverError: (_) => 'Server error, try again.',
+              ),
+            );
+          },
+          (success) {
+            authEvent.mapEventsToStates(
+              const AuthEvent.sendVerificationEmail(),
+            );
+            Modular.to.pushReplacementNamed('/verification');
+          },
+        ),
+      );
+    });
 
     return Form(
       child: Column(
