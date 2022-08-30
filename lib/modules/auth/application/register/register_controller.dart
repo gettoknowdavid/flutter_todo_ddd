7d6961db7d47fe9a27a90acacf0d58d646dd9045
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 01:48:50 +0100

    Create register_controller.dart

---
+++ lib/modules/auth/application/register/register_controller.dart
@@ -0,0 +1,65 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/register/register_event.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/register/register_state.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/errors/auth_failure.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/value_objects.dart';
+import 'package:flutter_todo_ddd/modules/auth/infrastructure/auth_facade.dart';
+
+class RegisterController extends StateNotifier<RegisterState> {
+  final AuthFacade _facade;
+
+  RegisterController(this._facade) : super(RegisterState.initial());
+
+  Future mapEventsToStates(RegisterEvent e) async {
+    return e.map(
+      nameChanged: _nameChanged,
+      emailChanged: _emailChanged,
+      passwordChanged: _passwordChanged,
+      registerPressed: _registerPressed,
+    );
+  }
+
+  _nameChanged(NameChanged e) async {
+    state = state.copyWith(
+      name: IName(e.name),
+      authOption: none(),
+    );
+  }
+
+  _emailChanged(EmailChanged e) async {
+    state = state.copyWith(
+      email: IEmail(e.email),
+      authOption: none(),
+    );
+  }
+
+  _passwordChanged(PasswordChanged e) async {
+    state = state.copyWith(
+      password: IPassword(e.password, isSignIn: true),
+      authOption: none(),
+    );
+  }
+
+  _registerPressed(RegisterPressed e) async {
+    Either<AuthFailure, Unit> r;
+
+    final nameValid = state.name.isValid();
+    final emailValid = state.email.isValid();
+    final passwordValid = state.password.isValid();
+
+    if (nameValid && emailValid && passwordValid) {
+      state = state.copyWith(loading: true, authOption: none());
+
+      r = await _facade.register(
+        name: state.name,
+        email: state.email,
+        password: state.password,
+      );
+
+      state = state.copyWith(loading: false, authOption: some(r));
+    }
+
+    state = state.copyWith(loading: false, showError: true, authOption: none());
+  }
+}
