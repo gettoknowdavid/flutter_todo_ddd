f88c35ba1feabd086a102904cb550ce1604c7cc6
David Michael II <knowdavidmichael@gmail.com>
2022-08-30 02:09:55 +0100

    Update auth_controller.dart

--- lib/modules/auth/application/auth_controller.dart
+++ lib/modules/auth/application/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
 import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
 
@@ -6,4 +7,47 @@ class AuthController extends StateNotifier<AuthState> {
   final IAuthFacade _facade;
 
   AuthController(this._facade) : super(const AuthState.initial());
+
+  Future mapEventsToStates(AuthEvent e) async {
+    return e.map(
+      checkRequested: _checkRequested,
+      checkVerified: _checkVerified,
+      sendVerificationEmail: _sendVerificationEmail,
+      loggedOut: _loggedOut,
+      openMailApp: _openMailApp,
+    );
+  }
+
+  _checkRequested(AuthCheckRequested e) async {
+    final option = await _facade.currentUser();
+    state = option.fold(
+      () => const AuthState.unauthenticated(),
+      (_) => const AuthState.authenticated(),
+    );
+  }
+
+  _checkVerified(AuthCheckVerified e) async {
+    final verifiedEither = await _facade.checkVerification();
+    state = verifiedEither.fold(
+      () => const AuthState.unauthenticated(),
+      (either) => either.fold(
+        (_) => const AuthState.unverified(),
+        (_) => const AuthState.verified(),
+      ),
+    );
+  }
+
+  _loggedOut(LoggedOut e) async {
+    await _facade.logout();
+    state = const AuthState.unauthenticated();
+  }
+
+  _openMailApp(OpenMailApp e) async {
+    throw UnimplementedError('To be implemented');
+  }
+
+  _sendVerificationEmail(SendVerificationEmail e) async {
+    await _facade.sendVerificationEmail();
+    state = const AuthState.awaitingVerified();
+  }
 }
