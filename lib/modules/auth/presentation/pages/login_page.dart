08ce01bc00bdd8032623bb65111f93174ae6da6e
David Michael II <knowdavidmichael@gmail.com>
2022-08-31 01:04:23 +0100

    add login page listener

--- lib/modules/auth/presentation/pages/login_page.dart
+++ lib/modules/auth/presentation/pages/login_page.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/login/login_event.dart';
 import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/login_form.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final event = ref.watch(loginProvider.notifier);
+    final authEvent = ref.watch(authProvider.notifier);
+    ref.listen<AuthState>(authProvider, (previous, next) {
+      next.maybeMap(
+        orElse: () => null,
+        authenticated: (_) {
+          print('Logged in');
+          Modular.to.pushReplacementNamed('/layout');
+        },
+      );
+    });
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: Center(
@@ -39,7 +56,12 @@ class LoginPage extends StatelessWidget {
                   height: SizeUtil.h(22),
                 ),
                 label: const Text('Login with Google'),
-                onPressed: () {},
+                onPressed: () {
+                  event.mapEventsToStates(
+                    const LoginEvent.googleLoginPressed(),
+                  );
+                  authEvent.mapEventsToStates(const AuthEvent.checkRequested());
+                },
               ),
               SizeUtil.vS(16),
               Row(
@@ -47,7 +69,10 @@ class LoginPage extends StatelessWidget {
                 children: [
                   const Text('Don\'t have an account?'),
                   TextButton(
-                    onPressed: () => Modular.to.pushNamed('/register'),
+                    onPressed: () {
+                      authEvent.mapEventsToStates(const AuthEvent.loggedOut());
+                      Modular.to.pushNamed('/register');
+                    },
                     style: TextButton.styleFrom(
                       padding: SizeUtil.pSymmetric(h: 6),
                     ),
