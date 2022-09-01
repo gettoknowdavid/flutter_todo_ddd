36cbe7721549c70f9184ebe85ff792af54374b5d
David Michael II <knowdavidmichael@gmail.com>
2022-09-01 11:53:39 +0100

    Update i_auth_facade.dart

--- lib/modules/auth/domain/i_auth_facade.dart
+++ lib/modules/auth/domain/i_auth_facade.dart
@@ -7,8 +7,6 @@ import 'package:flutter_todo_ddd/modules/auth/domain/value_objects.dart';
 abstract class IAuthFacade {
   Stream<firebase.User?> get authStateChange;
 
-  Stream<Option<firebase.User?>> authChange();
-
   Future<Option<Either<AuthFailure, bool?>>> checkVerification();
 
   Future<Option<User?>> currentUser();
