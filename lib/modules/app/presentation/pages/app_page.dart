9b7234e6d10c3271baa985f468f6b43139a62635
David Michael II <knowdavidmichael@gmail.com>
2022-09-01 11:17:35 +0100

    Update app_page.dart

--- lib/modules/app/presentation/pages/app_page.dart
+++ lib/modules/app/presentation/pages/app_page.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_button.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 
-class AppPae extends StatelessWidget {
-  const AppPae({Key? key}) : super(key: key);
+class AppPage extends ConsumerWidget {
+  const AppPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authEvent = ref.watch(authProvider.notifier);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('app Page Title'),
       ),
-      body: const Center(
-        child: Text('app Page Body'),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('app Page Body'),
+            AppButton(
+                onPressed: () {
+                  authEvent.mapEventsToStates(const AuthEvent.loggedOut());
+                },
+                title: 'Logout'),
+          ],
+        ),
       ),
     );
   }
