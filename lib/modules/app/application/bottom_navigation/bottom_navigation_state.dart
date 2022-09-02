aedf1d10933530ed1da3a4e8cf5a15e170b23741
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 20:27:50 +0100

    Create bottom_navigation_state.dart

---
+++ lib/modules/app/application/bottom_navigation/bottom_navigation_state.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_ddd/modules/app/domain/bottom_navigation.dart';
+import 'package:flutter_todo_ddd/modules/app/presentation/pages/app_page.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'bottom_navigation_state.freezed.dart';
+
+@freezed
+class BottomNavigationState with _$BottomNavigationState {
+  const factory BottomNavigationState({
+    required int currentIndex,
+    required Widget widget,
+    required List<BottomNavigation> items,
+  }) = _BottomNavigationState;
+
+  factory BottomNavigationState.initial() => BottomNavigationState(
+        currentIndex: 0,
+        widget: const AppPage(),
+        items: <BottomNavigation>[
+          BottomNavigation(title: 'Home', icon: Icons.home),
+          BottomNavigation(title: 'Calendar', icon: Icons.calendar_today),
+          BottomNavigation(title: 'Stats', icon: Icons.bar_chart),
+          BottomNavigation(title: 'Settings', icon: Icons.settings),
+        ],
+      );
+}
