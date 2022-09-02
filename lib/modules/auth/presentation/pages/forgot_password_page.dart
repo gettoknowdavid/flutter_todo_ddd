bd168b9fb9d0b2985849f1f8013fc1dbb8d9d9ae
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 19:26:19 +0100

    refactor form

--- lib/modules/auth/presentation/pages/forgot_password_page.dart
+++ lib/modules/auth/presentation/pages/forgot_password_page.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/forgot_password_form.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_button.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_snackbars.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_text_field.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/forgot-password/forgot_password_event.dart';
+import 'package:flutter_todo_ddd/modules/auth/application/forgot-password/forgot_password_state.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
-class ForgotPassword extends StatelessWidget {
-  const ForgotPassword({Key? key}) : super(key: key);
+class ForgotPasswordPage extends ConsumerWidget {
+  final formKey = GlobalKey<FormState>();
+
+  ForgotPasswordPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(forgotPasswordProvider);
+    final event = ref.watch(forgotPasswordProvider.notifier);
+
+    ref.listen<ForgotPasswordState>(forgotPasswordProvider, (previous, next) {
+      next.authOption.fold(
+        () => null,
+        (either) => either.fold(
+          (failure) => AppSnackbar.errorSnackbar(
+            title: 'Forgot Password Error',
+            message: failure.maybeMap(
+              orElse: () => '',
+              serverError: (_) => 'Server error, try again',
+              invalidEmail: (_) => 'The email provided is invalid.',
+              userNotFound: (_) => 'No user associated with this email.',
+            ),
+          ),
+          (success) {
+            event.mapEventsToStates(const ForgotPasswordEvent.statusChanged());
+          },
+        ),
+      );
+    });
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -33,17 +64,58 @@ class ForgotPassword extends StatelessWidget {
             children: [
               SizeUtil.vS(40),
               Image.asset(
-                'assets/images/forgot-password.png',
+                'assets/images/${state.status ? 'verify-email' : 'forgot-password'}.png',
                 height: SizeUtil.sh(0.4),
               ),
               SizeUtil.vS(20),
-              Text('Forgot Password ', style: AppTextStyles.authHeading),
               Text(
-                'Don\'t worry, it happens. Please enter the email address associated with your account.',
+                state.status ? 'You have mail' : 'Forgot Password ',
+                style: AppTextStyles.authHeading,
+              ),
+              Text(
+                state.status
+                    ? 'We\'ve sent you a mail with the password reset link. Please check your spam folder if you can\'t find the mail.'
+                    : 'Don\'t worry, it happens. Please enter the email address associated with your account.',
                 style: AppTextStyles.authSubheading,
               ),
               SizeUtil.vS(20),
-              ForgotPasswordForm(),
+              if (!state.status)
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      AppTextField(
+                        hint: 'johndoe@example.com',
+                        label: 'Email',
+                        enabled: !state.loading,
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) => event.mapEventsToStates(
+                          ForgotPasswordEvent.emailChanged(value),
+                        ),
+                        validator: (_) => state.email.value.fold(
+                          (f) => f.mapOrNull(
+                            invalidEmail: (_) => 'Please type in a valid email',
+                            empty: (_) => 'Email required',
+                          ),
+                          (_) => null,
+                        ),
+                      ),
+                      SizeUtil.vS(40),
+                      AppButton(
+                        disabled: !state.email.isValid(),
+                        loading: state.loading,
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            event.mapEventsToStates(
+                              const ForgotPasswordEvent.submitPressed(),
+                            );
+                          }
+                        },
+                        title: 'Submit',
+                      ),
+                    ],
+                  ),
+                ),
             ],
           ),
         ),
