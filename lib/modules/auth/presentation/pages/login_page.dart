99fd7bbe9ae0d7d7c58e3c2a9d62249af54701e6
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 06:07:11 +0100

    refactor: or_divider

--- lib/modules/auth/presentation/pages/login_page.dart
+++ lib/modules/auth/presentation/pages/login_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/login/login_event.dart';
 import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/login_form.dart';
+import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/or_divider.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
@@ -78,35 +79,4 @@ class LoginPage extends ConsumerWidget {
   }
 }
 
-class OrDivider extends StatelessWidget {
-  const OrDivider({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Expanded(
-          child: Container(
-            color: Colors.grey.shade300,
-            height: SizeUtil.h(1),
-          ),
-        ),
-        SizeUtil.hS(16),
-        Text(
-          'or',
-          style: AppTextStyles.authSubheading,
-          textAlign: TextAlign.center,
-        ),
-        SizeUtil.hS(16),
-        Expanded(
-          child: Container(
-            color: Colors.grey.shade300,
-            height: SizeUtil.h(1),
-          ),
-        ),
-      ],
-    );
-  }
-}
