059765f3030f95400efd71403bf2ffa21b2f16f1
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 20:31:09 +0100

    Create bottom_navigation_event.freezed.dart

---
+++ lib/modules/app/application/bottom_navigation/bottom_navigation_event.freezed.dart
@@ -0,0 +1,283 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'bottom_navigation_event.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$BottomNavigationEvent {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() indexSet,
+    required TResult Function() pageUpdated,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? indexSet,
+    TResult Function()? pageUpdated,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? indexSet,
+    TResult Function()? pageUpdated,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(IndexSet value) indexSet,
+    required TResult Function(PageUpdated value) pageUpdated,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(IndexSet value)? indexSet,
+    TResult Function(PageUpdated value)? pageUpdated,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(IndexSet value)? indexSet,
+    TResult Function(PageUpdated value)? pageUpdated,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BottomNavigationEventCopyWith<$Res> {
+  factory $BottomNavigationEventCopyWith(BottomNavigationEvent value,
+          $Res Function(BottomNavigationEvent) then) =
+      _$BottomNavigationEventCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$BottomNavigationEventCopyWithImpl<$Res>
+    implements $BottomNavigationEventCopyWith<$Res> {
+  _$BottomNavigationEventCopyWithImpl(this._value, this._then);
+
+  final BottomNavigationEvent _value;
+  // ignore: unused_field
+  final $Res Function(BottomNavigationEvent) _then;
+}
+
+/// @nodoc
+abstract class _$$IndexSetCopyWith<$Res> {
+  factory _$$IndexSetCopyWith(
+          _$IndexSet value, $Res Function(_$IndexSet) then) =
+      __$$IndexSetCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$IndexSetCopyWithImpl<$Res>
+    extends _$BottomNavigationEventCopyWithImpl<$Res>
+    implements _$$IndexSetCopyWith<$Res> {
+  __$$IndexSetCopyWithImpl(_$IndexSet _value, $Res Function(_$IndexSet) _then)
+      : super(_value, (v) => _then(v as _$IndexSet));
+
+  @override
+  _$IndexSet get _value => super._value as _$IndexSet;
+}
+
+/// @nodoc
+
+class _$IndexSet implements IndexSet {
+  const _$IndexSet();
+
+  @override
+  String toString() {
+    return 'BottomNavigationEvent.indexSet()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$IndexSet);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() indexSet,
+    required TResult Function() pageUpdated,
+  }) {
+    return indexSet();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? indexSet,
+    TResult Function()? pageUpdated,
+  }) {
+    return indexSet?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? indexSet,
+    TResult Function()? pageUpdated,
+    required TResult orElse(),
+  }) {
+    if (indexSet != null) {
+      return indexSet();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(IndexSet value) indexSet,
+    required TResult Function(PageUpdated value) pageUpdated,
+  }) {
+    return indexSet(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(IndexSet value)? indexSet,
+    TResult Function(PageUpdated value)? pageUpdated,
+  }) {
+    return indexSet?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(IndexSet value)? indexSet,
+    TResult Function(PageUpdated value)? pageUpdated,
+    required TResult orElse(),
+  }) {
+    if (indexSet != null) {
+      return indexSet(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class IndexSet implements BottomNavigationEvent {
+  const factory IndexSet() = _$IndexSet;
+}
+
+/// @nodoc
+abstract class _$$PageUpdatedCopyWith<$Res> {
+  factory _$$PageUpdatedCopyWith(
+          _$PageUpdated value, $Res Function(_$PageUpdated) then) =
+      __$$PageUpdatedCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$PageUpdatedCopyWithImpl<$Res>
+    extends _$BottomNavigationEventCopyWithImpl<$Res>
+    implements _$$PageUpdatedCopyWith<$Res> {
+  __$$PageUpdatedCopyWithImpl(
+      _$PageUpdated _value, $Res Function(_$PageUpdated) _then)
+      : super(_value, (v) => _then(v as _$PageUpdated));
+
+  @override
+  _$PageUpdated get _value => super._value as _$PageUpdated;
+}
+
+/// @nodoc
+
+class _$PageUpdated implements PageUpdated {
+  const _$PageUpdated();
+
+  @override
+  String toString() {
+    return 'BottomNavigationEvent.pageUpdated()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$PageUpdated);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() indexSet,
+    required TResult Function() pageUpdated,
+  }) {
+    return pageUpdated();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? indexSet,
+    TResult Function()? pageUpdated,
+  }) {
+    return pageUpdated?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? indexSet,
+    TResult Function()? pageUpdated,
+    required TResult orElse(),
+  }) {
+    if (pageUpdated != null) {
+      return pageUpdated();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(IndexSet value) indexSet,
+    required TResult Function(PageUpdated value) pageUpdated,
+  }) {
+    return pageUpdated(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(IndexSet value)? indexSet,
+    TResult Function(PageUpdated value)? pageUpdated,
+  }) {
+    return pageUpdated?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(IndexSet value)? indexSet,
+    TResult Function(PageUpdated value)? pageUpdated,
+    required TResult orElse(),
+  }) {
+    if (pageUpdated != null) {
+      return pageUpdated(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class PageUpdated implements BottomNavigationEvent {
+  const factory PageUpdated() = _$PageUpdated;
+}
