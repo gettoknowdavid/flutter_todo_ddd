3a99dac391dd3114f5b1037f317590621987287c
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 16:09:34 +0100

    Create forgot_password_event.dart

---
+++ lib/modules/auth/application/forgot-password/forgot_password_event.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'forgot_password_event.freezed.dart';
+
+@freezed
+class ForgotPasswordEvent with _$ForgotPasswordEvent {
+  const factory ForgotPasswordEvent.emailChanged(String email) = EmailChanged;
+  const factory ForgotPasswordEvent.submitPressed(String email) = SubmitPressed;
+}
