200eaef2fcd1cf6a8d9cfcb1bc5c2dcda713077b
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 06:21:19 +0100

    add back button and illustration

--- lib/modules/auth/presentation/pages/register_page.dart
+++ lib/modules/auth/presentation/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_todo_ddd/modules/auth/presentation/widgets/register_form.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
@@ -10,21 +11,37 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: const SizedBox(),
+        leadingWidth: 0,
+        backgroundColor: Colors.transparent,
+        title: TextButton.icon(
+          icon: const BackButtonIcon(),
+          label: const Text('Back to Login'),
+          onPressed: () => Modular.to.pop(),
+          style: TextButton.styleFrom(
+            backgroundColor: Theme.of(context).primaryColorLight,
+            padding: SizeUtil.pFromLTRB(12, 0, 12, 0),
+          ),
+        ),
+      ),
       body: Center(
         child: SingleChildScrollView(
-          padding: SizeUtil.pSymmetric(h: 18, v: 20),
+          padding: SizeUtil.pFromLTRB(18, 40, 18, 8),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizeUtil.vS(90),
-              Text('Hello.', style: AppTextStyles.authHeading),
-              SizeUtil.vS(6),
+              SizeUtil.vS(30),
+              Image.asset(
+                'assets/images/register.png',
+                height: SizeUtil.sh(0.4),
+              ),
+              Text('Register', style: AppTextStyles.authHeading),
               Text(
-                'You can create your new account with your email and password.',
+                'Register with your email and password.',
                 style: AppTextStyles.authSubheading,
               ),
-              SizeUtil.vS(36),
+              SizeUtil.vS(20),
               RegisterForm(),
             ],
           ),
