7eeed1f3640d3d842b018b1fd181d797f6ff2301
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 15:39:07 +0100

    add _ForgotPasswordForm widget

--- lib/modules/auth/presentation/pages/forgot_password.dart
+++ lib/modules/auth/presentation/pages/forgot_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_button.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_text_field.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
@@ -26,21 +28,55 @@ class ForgotPassword extends StatelessWidget {
       ),
       body: Center(
         child: SingleChildScrollView(
+          padding: SizeUtil.pFromLTRB(18, 0, 18, 8),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizeUtil.vS(30),
+              SizeUtil.vS(40),
               Image.asset(
                 'assets/images/forgot-password.png',
                 height: SizeUtil.sh(0.4),
               ),
+              SizeUtil.vS(20),
               Text('Forgot Password ', style: AppTextStyles.authHeading),
               Text(
                 'Don\'t worry, it happens. Please enter the email address associated with your account.',
                 style: AppTextStyles.authSubheading,
               ),
+              SizeUtil.vS(20),
+              _ForgotPasswordForm(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _ForgotPasswordForm extends StatelessWidget {
+  _ForgotPasswordForm({Key? key}) : super(key: key);
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          AppTextField(
+            hint: 'johndoe@example.com',
+            label: 'Email',
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) {},
+          ),
+          SizeUtil.vS(40),
+          AppButton(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {}
+            },
+            title: 'Submit',
+          ),
+        ],
       ),
     );
   }
