078d77a35a5f0b977ad91a228fd1c6d55c80ddf8
David Michael II <knowdavidmichael@gmail.com>
2022-09-02 06:06:56 +0100

    Create or_divider.dart

---
+++ lib/modules/auth/presentation/widgets/or_divider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
+import 'package:flutter_todo_ddd/utils/size_util.dart';
+
+class OrDivider extends StatelessWidget {
+  const OrDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.grey.shade300,
+            height: SizeUtil.h(1),
+          ),
+        ),
+        SizeUtil.hS(16),
+        Text(
+          'or',
+          style: AppTextStyles.authSubheading,
+          textAlign: TextAlign.center,
+        ),
+        SizeUtil.hS(16),
+        Expanded(
+          child: Container(
+            color: Colors.grey.shade300,
+            height: SizeUtil.h(1),
+          ),
+        ),
+      ],
+    );
+  }
+}
