312c80f65206085952b64368dd070463edbfce30
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 09:52:05 +0100

    feat: improve AppIcon in bottom navigation bar

--- lib/modules/app/presentation/widgets/bottom_navigation_item.dart
+++ lib/modules/app/presentation/widgets/bottom_navigation_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_icon.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_providers.dart';
 import 'package:flutter_todo_ddd/modules/app/domain/bottom_navigation.dart';
+import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class BottomNavigationItem extends ConsumerWidget {
   const BottomNavigationItem({
@@ -16,27 +18,38 @@ class BottomNavigationItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final bottomNavState = ref.watch(bottomNavProvider);
-    final bottomNavEvent = ref.watch(bottomNavProvider.notifier);
+    final index = bottomNavState.pages.indexOf(item);
+    final selected = bottomNavState.currentIndex == index;
 
     final theme = Theme.of(context);
-    final bNBTheme = Theme.of(context).bottomNavigationBarTheme;
 
-    final index = bottomNavState.pages.indexOf(item);
-    final color = bottomNavState.currentIndex == index
-        ? bNBTheme.selectedItemColor
-        : bNBTheme.unselectedItemColor;
+    final color = selected
+        ? theme.bottomNavigationBarTheme.selectedItemColor
+        : theme.bottomNavigationBarTheme.unselectedItemColor;
 
     return GestureDetector(
       onTap: onSelect,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(
-            item.icon,
-            color: color,
-          ),
-        ],
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        height: SizeUtil.h(44),
+        padding: SizeUtil.pOnly(right: 16),
+        decoration: BoxDecoration(
+          color: selected ? theme.colorScheme.secondary : null,
+          borderRadius: SizeUtil.borderRadius(16),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AppIcon(
+              icon: item.icon,
+              color: color,
+              semantic: item.icon,
+              height: SizeUtil.h(24),
+              width: SizeUtil.w(24),
+            ),
+            if (selected) Text(item.title, style: TextStyle(color: color)),
+          ],
+        ),
       ),
     );
   }
