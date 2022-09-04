1260dbd7c9070c7e7382f074aa8f23681574f965
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 10:41:10 +0100

    fix: linting

--- lib/modules/app/presentation/pages/app_layout.dart
+++ lib/modules/app/presentation/pages/app_layout.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_providers.dart';
-import 'package:flutter_todo_ddd/modules/app/application/bottom_navigation/bottom_navigation_event.dart';
-import 'package:flutter_todo_ddd/modules/app/presentation/widgets/bottom_navigation_item.dart';
+import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_bottom_navigation_bar.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
-import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class AppLayout extends ConsumerWidget {
   const AppLayout({Key? key}) : super(key: key);
@@ -26,45 +24,6 @@ class AppLayout extends ConsumerWidget {
       appBar: AppBar(title: const Text('Teek Layout')),
       body: bottomNavState.page,
       bottomNavigationBar: const AppBottomNavigationBar(),
-    );
-  }
-}
-
-class AppBottomNavigationBar extends ConsumerWidget {
-  const AppBottomNavigationBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final bottomNavState = ref.watch(bottomNavProvider);
-    final bottomNavEvent = ref.watch(bottomNavProvider.notifier);
-
-    int pagesLength = bottomNavState.pages.length;
-
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 250),
-      width: SizeUtil.sw(1),
-      height: SizeUtil.h(80),
-      // color: Colors.pink,
-      padding: SizeUtil.pSymmetric(h: 18),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          for (var i = 0; i < pagesLength; i++) ...[
-            BottomNavigationItem(
-              item: bottomNavState.pages[i],
-              onSelect: () {
-                final index = bottomNavState.pages.indexOf(
-                  bottomNavState.pages[i],
-                );
-
-                bottomNavEvent.mapEventsToStates(
-                  BottomNavigationEvent.pageUpdated(index),
-                );
-              },
-            )
-          ],
-        ],
-      ),
     );
   }
 }
