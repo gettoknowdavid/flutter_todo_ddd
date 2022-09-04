922054f253870b4b81b00cb7e1c8e9ceb224b5d4
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 09:48:30 +0100

    add app icon constants

--- lib/common/constants/app_icons.dart
+++ lib/common/constants/app_icons.dart
@@ -1,7 +1,29 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
 class AppIcons {
   AppIcons._();
 
-  static const String show = 'eye-open';
-  static const String hide = 'eye-close';
+  static const String arrowLeft = 'arrow-left';
+  static const String calendar = 'calendar';
+  static const String camera = 'camera';
+  static const String category = 'category';
+  static const String chart = 'chart';
+  static const String delete = 'delete';
+  static const String document = 'document';
+  static const String editSquare = 'edit-square';
+  static const String filter = 'filter';
+  static const String folder = 'folder';
   static const String google = 'google';
+  static const String heart = 'heart';
+  static const String hide = 'hide';
+  static const String home = 'home';
+  static const String image = 'image';
+  static const String logout = 'logout';
+  static const String moreSquare = 'more-square';
+  static const String notification = 'notification';
+  static const String paperPlus = 'paper-plus';
+  static const String plus = 'plus';
+  static const String profile = 'profile';
+  static const String setting = 'setting';
+  static const String show = 'show';
 }
