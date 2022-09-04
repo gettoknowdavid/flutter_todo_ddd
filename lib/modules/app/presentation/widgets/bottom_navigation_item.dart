367f54f183164c23542cf999d8b9e96bc9c6ec78
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 10:17:03 +0100

    refactor: Add icon widget

--- lib/modules/app/presentation/widgets/bottom_navigation_item.dart
+++ lib/modules/app/presentation/widgets/bottom_navigation_item.dart
@@ -6,14 +6,14 @@ import 'package:flutter_todo_ddd/modules/app/domain/bottom_navigation.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class BottomNavigationItem extends ConsumerWidget {
+  final BottomNavigation item;
+
+  final void Function()? onSelect;
   const BottomNavigationItem({
     Key? key,
     required this.item,
     required this.onSelect,
   }) : super(key: key);
-
-  final BottomNavigation item;
-  final void Function()? onSelect;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,36 +21,48 @@ class BottomNavigationItem extends ConsumerWidget {
     final index = bottomNavState.pages.indexOf(item);
     final selected = bottomNavState.currentIndex == index;
 
-    final theme = Theme.of(context);
-
-    final color = selected
-        ? theme.bottomNavigationBarTheme.selectedItemColor
-        : theme.bottomNavigationBarTheme.unselectedItemColor;
+    final boxColor = selected ? const Color.fromRGBO(255, 133, 76, 0.3) : null;
 
     return GestureDetector(
       onTap: onSelect,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 250),
-        height: SizeUtil.h(44),
-        padding: SizeUtil.pOnly(right: 16),
-        decoration: BoxDecoration(
-          color: selected ? theme.colorScheme.secondary : null,
-          borderRadius: SizeUtil.borderRadius(16),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AppIcon(
-              icon: item.icon,
-              color: color,
-              semantic: item.icon,
-              height: SizeUtil.h(24),
-              width: SizeUtil.w(24),
+      child: index == 2
+          ? const _AddIcon()
+          : AnimatedContainer(
+              duration: const Duration(milliseconds: 250),
+              height: SizeUtil.h(44),
+              width: SizeUtil.h(44),
+              decoration: BoxDecoration(
+                borderRadius: SizeUtil.borderRadius(16),
+                color: boxColor,
+              ),
+              child: AppIcon(
+                icon: item.icon,
+                semantic: item.icon,
+                height: SizeUtil.h(24),
+                width: SizeUtil.w(24),
+              ),
             ),
-            if (selected) Text(item.title, style: TextStyle(color: color)),
-          ],
-        ),
+    );
+  }
+}
+
+class _AddIcon extends StatelessWidget {
+  const _AddIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final addIconColor = theme.bottomNavigationBarTheme.selectedItemColor;
+
+    return Container(
+      height: SizeUtil.h(60),
+      width: SizeUtil.h(60),
+      decoration: BoxDecoration(
+        color: theme.colorScheme.secondary,
+        borderRadius: SizeUtil.borderRadius(20),
       ),
+      child: Icon(Icons.add, color: addIconColor),
     );
   }
 }
