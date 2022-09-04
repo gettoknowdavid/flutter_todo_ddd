5beea98fc7669c366806f2854e96b26e99de8657
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 23:20:10 +0100

    fix: linting

--- lib/modules/todo/presentation/pages/profile_page.dart
+++ lib/modules/todo/presentation/pages/profile_page.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_todo_ddd/common/constants/app_icons.dart';
-import 'package:flutter_todo_ddd/common/widgets/app_icon.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_back_button.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/auth_controller.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
-import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/logout_button.dart';
-import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class ProfilePage extends ConsumerWidget {
   const ProfilePage({Key? key}) : super(key: key);
