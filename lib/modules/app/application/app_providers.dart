ef8c5cbad44d9c3d37895f8ab5620c5d94b37e74
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 11:06:03 +0100

    add appProvider to app_providers

--- lib/modules/app/application/app_providers.dart
+++ lib/modules/app/application/app_providers.dart
@@ -1,6 +1,14 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/app/application/app_controller.dart';
+import 'package:flutter_todo_ddd/modules/app/application/app_state.dart';
 import 'package:flutter_todo_ddd/modules/app/application/bottom_navigation/bottom_navigation_controller.dart';
 import 'package:flutter_todo_ddd/modules/app/application/bottom_navigation/bottom_navigation_state.dart';
+import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
+
+final appProvider = StateNotifierProvider<AppController, AppState>(
+  (ref) => AppController(Modular.get<IAuthFacade>()),
+);
 
 final bottomNavProvider =
     StateNotifierProvider<BottomNavigationController, BottomNavigationState>(
