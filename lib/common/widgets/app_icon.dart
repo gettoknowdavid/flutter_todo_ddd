f658b95f3f09d9161ca6638cfc8650c99d53919a
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 09:49:20 +0100

    fix: change AppIcon to Svg with height and width

--- lib/common/widgets/app_icon.dart
+++ lib/common/widgets/app_icon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class AppIcon extends StatelessWidget {
@@ -9,6 +10,8 @@ class AppIcon extends StatelessWidget {
     this.padding,
     this.color,
     this.size,
+    this.height,
+    this.width,
     required this.semantic,
   }) : super(key: key);
 
@@ -17,21 +20,27 @@ class AppIcon extends StatelessWidget {
   final EdgeInsetsGeometry? padding;
   final Color? color;
   final double? size;
+  final double? height;
+  final double? width;
+
   final String semantic;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
+    final assetName = 'assets/icons/$icon.svg';
+
     return IconButton(
       padding: padding ?? SizeUtil.pAll(0),
       onPressed: onPressed,
       splashColor: Colors.transparent,
-      icon: ImageIcon(
-        AssetImage('assets/icons/$icon.png'),
+      icon: SvgPicture.asset(
+        assetName,
+        semanticsLabel: semantic,
         color: color ?? theme.iconTheme.color,
-        semanticLabel: semantic,
-        size: size ?? theme.iconTheme.size,
+        height: height,
+        width: width,
       ),
     );
   }
