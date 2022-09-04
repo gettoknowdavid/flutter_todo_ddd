d286162a9045214d03f7e4db17cfba599bb63595
David Michael II <knowdavidmichael@gmail.com>
2022-09-04 21:53:01 +0100

    add AppState initialization in SplashScreen listener

--- lib/modules/app/presentation/pages/splash_page.dart
+++ lib/modules/app/presentation/pages/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/app/application/app_event.dart';
+import 'package:flutter_todo_ddd/modules/app/application/app_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_event.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
@@ -15,10 +17,14 @@ class SplashPage extends ConsumerStatefulWidget {
 class _SplashPageState extends ConsumerState<SplashPage> {
   @override
   Widget build(BuildContext context) {
+    final appEvent = ref.watch(appProvider.notifier);
     ref.listen<AuthState>(authProvider, (previous, next) {
       Future.delayed(const Duration(seconds: 5), () {
         next.mapOrNull(
-          authenticated: (_) => Modular.to.pushReplacementNamed('/layout'),
+          authenticated: (_) {
+            appEvent.mapEventsToStates(const AppEvent.initialized());
+            Modular.to.pushReplacementNamed('/layout');
+          },
           unauthenticated: (_) => Modular.to.pushReplacementNamed('/login'),
           unverified: (_) => Modular.to.pushReplacementNamed('/verification'),
         );
