b80b77578f5bc90371f6743e4d8109892c81b8c2
David Michael II <knowdavidmichael@gmail.com>
2022-09-05 16:30:10 +0100

    add InsufficientPermission TodoFailure

--- lib/modules/todo/domain/errors/todo_failure.dart
+++ lib/modules/todo/domain/errors/todo_failure.dart
@@ -4,5 +4,7 @@ part 'todo_failure.freezed.dart';
 
 @freezed
 class TodoFailure with _$TodoFailure {
+  const factory TodoFailure.insufficientPermissions() =
+      _TodoInsufficientPermissions;
   const factory TodoFailure.serverError() = _TodoServerError;
 }
