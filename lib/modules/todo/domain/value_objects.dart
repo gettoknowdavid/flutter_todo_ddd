d1349f1ae8ad87f25411f21bd7758524de14c705
David Michael II <knowdavidmichael@gmail.com>
2022-09-05 16:07:42 +0100

    fix: value validators file location

--- lib/modules/todo/domain/value_objects.dart
+++ lib/modules/todo/domain/value_objects.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_todo_ddd/core/value_failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_todo_ddd/core/value_object.dart';
-import 'package:flutter_todo_ddd/modules/todo/domain/entities/value_validators.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/value_validators.dart';
 
 class ITodoTitle extends ValueObject<String> {
   @override
