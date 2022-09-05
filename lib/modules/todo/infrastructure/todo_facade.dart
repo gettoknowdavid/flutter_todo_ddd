fc77bc93b2d05a36b9bf62b6196a7dcc47114e2a
David Michael II <knowdavidmichael@gmail.com>
2022-09-05 16:16:17 +0100

    create TodoFacade

---
+++ lib/modules/todo/infrastructure/todo_facade.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_todo_ddd/modules/todo/domain/errors/todo_failure.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/entities/todo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/i_todo_facade.dart';
+
+class TodoFacade implements ITodoFacade {
+  @override
+  Future<Either<TodoFailure, Unit>> create(Todo todo) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<TodoFailure, Unit>> delete(Todo todo) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<TodoFailure, Unit>> edit(Todo todo) {
+    // TODO: implement edit
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<TodoFailure, Unit>> update(Todo todo) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<Either<TodoFailure, List<Todo>>> watchAll() {
+    // TODO: implement watchAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<Either<TodoFailure, List<Todo>>> watchUncompleted() {
+    // TODO: implement watchUncompleted
+    throw UnimplementedError();
+  }
+}
