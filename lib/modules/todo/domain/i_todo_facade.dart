55c5aebbf47317f3df13649cb69961d23ddc411b
David Michael II <knowdavidmichael@gmail.com>
2022-09-05 16:06:42 +0100

    create ITodoFacade with contracts
    create, delete, edit and update todo future contract with watchAll and watchUncompleted stream contracts

---
+++ lib/modules/todo/domain/i_todo_facade.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/entities/todo.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/errors/todo_failure.dart';
+
+abstract class ITodoFacade {
+  Future<Either<TodoFailure, Unit>> create(Todo todo);
+  Future<Either<TodoFailure, Unit>> delete(Todo todo);
+  Future<Either<TodoFailure, Unit>> edit(Todo todo);
+  Future<Either<TodoFailure, Unit>> update(Todo todo);
+  Stream<Either<TodoFailure, List<Todo>>> watchAll();
+  Stream<Either<TodoFailure, List<Todo>>> watchUncompleted();
+}
