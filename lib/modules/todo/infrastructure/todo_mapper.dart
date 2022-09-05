a77d5c658cde84373f6806f0ca93246cb5c047e9
David Michael II <knowdavidmichael@gmail.com>
2022-09-05 18:25:14 +0100

    Create todo_mapper.dart

---
+++ lib/modules/todo/infrastructure/todo_mapper.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_todo_ddd/core/uid.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/entities/todo.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/value_objects.dart';
+import 'package:flutter_todo_ddd/modules/todo/infrastructure/dtos/todo_dto.dart';
+
+class TodoMapper {
+  Todo? toDomain(TodoDto? dto) {
+    if (dto == null) {
+      return null;
+    }
+
+    return Todo(
+      uid: Uid.fromUniqueString(dto.uid),
+      title: ITodoTitle(dto.title),
+      isDone: dto.isDone,
+      description: dto.description,
+      time: dto.time,
+      createdAt: dto.createdAt,
+    );
+  }
+}
