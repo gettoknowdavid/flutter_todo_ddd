219a2ff28f9ff554ea64ac90a6bf110f2cbb8333
David Michael II <knowdavidmichael@gmail.com>
2022-09-05 16:05:46 +0100

    Create todo_failure.freezed.dart

---
+++ lib/modules/todo/domain/errors/todo_failure.freezed.dart
@@ -0,0 +1,166 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'todo_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$TodoFailure {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() serverError,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? serverError,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? serverError,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_TodoServerError value) serverError,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_TodoServerError value)? serverError,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_TodoServerError value)? serverError,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TodoFailureCopyWith<$Res> {
+  factory $TodoFailureCopyWith(
+          TodoFailure value, $Res Function(TodoFailure) then) =
+      _$TodoFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$TodoFailureCopyWithImpl<$Res> implements $TodoFailureCopyWith<$Res> {
+  _$TodoFailureCopyWithImpl(this._value, this._then);
+
+  final TodoFailure _value;
+  // ignore: unused_field
+  final $Res Function(TodoFailure) _then;
+}
+
+/// @nodoc
+abstract class _$$_TodoServerErrorCopyWith<$Res> {
+  factory _$$_TodoServerErrorCopyWith(
+          _$_TodoServerError value, $Res Function(_$_TodoServerError) then) =
+      __$$_TodoServerErrorCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_TodoServerErrorCopyWithImpl<$Res>
+    extends _$TodoFailureCopyWithImpl<$Res>
+    implements _$$_TodoServerErrorCopyWith<$Res> {
+  __$$_TodoServerErrorCopyWithImpl(
+      _$_TodoServerError _value, $Res Function(_$_TodoServerError) _then)
+      : super(_value, (v) => _then(v as _$_TodoServerError));
+
+  @override
+  _$_TodoServerError get _value => super._value as _$_TodoServerError;
+}
+
+/// @nodoc
+
+class _$_TodoServerError implements _TodoServerError {
+  const _$_TodoServerError();
+
+  @override
+  String toString() {
+    return 'TodoFailure.serverError()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_TodoServerError);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() serverError,
+  }) {
+    return serverError();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? serverError,
+  }) {
+    return serverError?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? serverError,
+    required TResult orElse(),
+  }) {
+    if (serverError != null) {
+      return serverError();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_TodoServerError value) serverError,
+  }) {
+    return serverError(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_TodoServerError value)? serverError,
+  }) {
+    return serverError?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_TodoServerError value)? serverError,
+    required TResult orElse(),
+  }) {
+    if (serverError != null) {
+      return serverError(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _TodoServerError implements TodoFailure {
+  const factory _TodoServerError() = _$_TodoServerError;
+}
