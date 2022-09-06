91592211821dcdb1ca987918d7082e3a002ce8db
David Michael II <knowdavidmichael@gmail.com>
2022-09-06 23:26:39 +0100

    fix imports

--- lib/modules/todo/application/todo_provider.dart
+++ lib/modules/todo/application/todo_provider.dart
@@ -1,7 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/todo_form/todo_form_controller.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/i_todo_facade.dart';
 
 final todoFormProvider =
     StateNotifierProvider.autoDispose<TodoFormController, TodoFormState>(
-  (ref) => TodoFormController(),
+  (ref) => TodoFormController(Modular.get<ITodoFacade>()),
 );
