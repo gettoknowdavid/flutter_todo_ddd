ae306f5c3b83cdbc03aa7de8ec7c06492810714d
David Michael II <knowdavidmichael@gmail.com>
2022-09-06 15:54:55 +0100

    create todo_form_event

---
+++ lib/modules/todo/application/todo_form/todo_form_event.dart
@@ -0,0 +1,12 @@
+part of 'todo_form_controller.dart';
+
+@freezed
+class TodoFormEvent with _$TodoFormEvent {
+  const factory TodoFormEvent.descChanged(String desc) = _TodoDescChanged;
+  const factory TodoFormEvent.initialized(Option<Todo> initialOption) =
+      _TodoFormInitialized;
+  const factory TodoFormEvent.isDoneChanged(bool isDone) = _TodoIsDoneChanged;
+  const factory TodoFormEvent.saved() = _TodoSaved;
+  const factory TodoFormEvent.timeChanged(DateTime time) = _TodoTimeChanged;
+  const factory TodoFormEvent.titleChanged(String title) = _TodoTitleChanged;
+}
