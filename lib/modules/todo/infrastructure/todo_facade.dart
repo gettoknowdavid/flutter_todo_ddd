45a50143abf42daaec9064027a6a3a2dc4a0d451
David Michael II <knowdavidmichael@gmail.com>
2022-09-06 14:07:11 +0100

    add watchAll contract implementation with TodoMapper

--- lib/modules/todo/infrastructure/todo_facade.dart
+++ lib/modules/todo/infrastructure/todo_facade.dart
@@ -5,8 +5,14 @@ import 'package:flutter_todo_ddd/modules/todo/domain/entities/todo.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/errors/todo_failure.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/i_todo_facade.dart';
 import 'package:flutter_todo_ddd/modules/todo/infrastructure/dtos/todo_dto.dart';
+import 'package:flutter_todo_ddd/modules/todo/infrastructure/todo_mapper.dart';
+import 'package:rxdart/rxdart.dart';
 
 class TodoFacade implements ITodoFacade {
+  final TodoMapper _mapper;
+
+  TodoFacade(this._mapper);
+
   @override
   Future<Either<TodoFailure, Unit>> create(Todo todo) async {
     final todoDto = TodoDto.fromDomain(todo);
@@ -59,19 +65,23 @@ class TodoFacade implements ITodoFacade {
   }
 
   @override
-  Future<Either<TodoFailure, Unit>> update(Todo todo) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Stream<Either<TodoFailure, List<Todo?>>> watchAll() async* {
+    yield* todosRef
+        .orderByFieldPath(FieldPath.fromString('serverTimeStamp'))
+        .snapshots()
+        .map((snapshot) => right<TodoFailure, List<Todo?>>(
+            snapshot.docs.map((doc) => _mapper.toDomain(doc.data)).toList()))
+        .onErrorReturnWith((e, stackTrace) {
+      if (e is PlatformException && e.message!.contains('PERMISSION_DENIED')) {
+        return left(const TodoFailure.insufficientPermissions());
+      } else {
+        return left(const TodoFailure.serverError());
+      }
+    });
   }
 
   @override
-  Stream<Either<TodoFailure, List<Todo>>> watchAll() {
-    // TODO: implement watchAll
-    throw UnimplementedError();
-  }
-
-  @override
-  Stream<Either<TodoFailure, List<Todo>>> watchUncompleted() {
+  Stream<Either<TodoFailure, List<Todo?>>> watchUncompleted() {
     // TODO: implement watchUncompleted
     throw UnimplementedError();
   }
