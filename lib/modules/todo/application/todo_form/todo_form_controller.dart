133a0063aa17944f828535f26343c6723510b12f
David Michael II <knowdavidmichael@gmail.com>
2022-09-06 23:27:29 +0100

    add maps

--- lib/modules/todo/application/todo_form/todo_form_controller.dart
+++ lib/modules/todo/application/todo_form/todo_form_controller.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/entities/todo.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/errors/todo_failure.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/i_todo_facade.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/value_objects.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'todo_form_controller.freezed.dart';
@@ -9,7 +11,9 @@ part 'todo_form_event.dart';
 part 'todo_form_state.dart';
 
 class TodoFormController extends StateNotifier<TodoFormState> {
-  TodoFormController() : super(TodoFormState.initial());
+  final ITodoFacade _facade;
+
+  TodoFormController(this._facade) : super(TodoFormState.initial());
 
   Future mapEventsToStates(TodoFormEvent e) async {
     return e.map(
@@ -22,7 +26,13 @@ class TodoFormController extends StateNotifier<TodoFormState> {
     );
   }
 
-  _descChanged(_TodoDescChanged e) async {}
+  _descChanged(_TodoDescChanged e) async {
+    state = state.copyWith(
+      todo: state.todo.copyWith(
+        description: e.desc,
+      ),
+    );
+  }
 
   _initialized(_TodoFormInitialized e) async {
     e.initialOption.fold(() => state, (initialTodo) {
@@ -33,8 +43,43 @@ class TodoFormController extends StateNotifier<TodoFormState> {
     });
   }
 
-  _isDoneChanged(_TodoIsDoneChanged e) async {}
-  _saved(_TodoSaved e) async {}
-  _timeChanged(_TodoTimeChanged e) async {}
-  _titleChanged(_TodoTitleChanged e) async {}
+  _isDoneChanged(_TodoIsDoneChanged e) async {
+    state = state.copyWith(
+      todo: state.todo.copyWith(
+        isDone: e.isDone,
+      ),
+    );
+  }
+
+  _saved(_TodoSaved e) async {
+    Either<TodoFailure, Unit> r;
+
+    state = state.copyWith(loading: true, option: none());
+
+    r = state.isEdit
+        ? await _facade.edit(state.todo)
+        : await _facade.create(state.todo);
+
+    state = state.copyWith(
+      loading: false,
+      showErrors: true,
+      option: optionOf(r),
+    );
+  }
+
+  _timeChanged(_TodoTimeChanged e) async {
+    state = state.copyWith(
+      todo: state.todo.copyWith(
+        time: e.time,
+      ),
+    );
+  }
+
+  _titleChanged(_TodoTitleChanged e) async {
+    state = state.copyWith(
+      todo: state.todo.copyWith(
+        title: ITodoTitle(e.title),
+      ),
+    );
+  }
 }
