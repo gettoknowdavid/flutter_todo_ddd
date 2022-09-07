cc2bffbd8a6af17c3848812e0cb53554728aeebb
David Michael II <knowdavidmichael@gmail.com>
2022-09-07 01:19:16 +0100

    refactor: SearchBox widget

--- lib/modules/todo/presentation/pages/home_page.dart
+++ lib/modules/todo/presentation/pages/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_todo_ddd/common/constants/app_icons.dart';
-import 'package:flutter_todo_ddd/common/widgets/app_icon.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_providers.dart';
+import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/search_box.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/get_greeting.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
@@ -22,9 +21,8 @@ class HomePage extends ConsumerWidget {
             SizeUtil.vS(20),
             const _NameHeader(),
             SizeUtil.vS(30),
-            const SearchBar(),
+            const SearchBox(),
             SizeUtil.vS(30),
-
           ],
         ),
       ),
@@ -53,31 +51,6 @@ class _NameHeader extends ConsumerWidget {
           Text(text, style: AppTextStyles.homeNameHeader),
           Text(greeting, style: AppTextStyles.homeHeaderGreeting),
         ],
-      ),
-    );
-  }
-}
-
-class SearchBar extends StatelessWidget {
-  const SearchBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Padding(
-      padding: SizeUtil.pSymmetric(h: 18),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Search for todos',
-          prefixIcon: AppIcon(
-            padding: SizeUtil.pSymmetric(h: 16),
-            icon: AppIcons.search,
-            semantic: 'Search icon',
-            height: SizeUtil.h(22),
-            color: theme.primaryColorDark,
-          ),
-        ),
       ),
     );
   }
