5d72ba52c3e22e2c89bbe399077640bf8fd669b3
David Michael II <knowdavidmichael@gmail.com>
2022-09-07 23:16:43 +0100

    add CreateTodoPage to app router

--- lib/modules/todo/presentation/pages/create_todo_page.dart
+++ lib/modules/todo/presentation/pages/create_todo_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_app_bar.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
-class CreateTodo extends StatelessWidget {
-  const CreateTodo({super.key});
+class CreateTodoPage extends StatelessWidget {
+  const CreateTodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
