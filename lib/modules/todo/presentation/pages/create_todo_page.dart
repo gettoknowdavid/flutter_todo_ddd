b2fe319fa5b15157577a12b05b905bcacf2da538
David Michael II <knowdavidmichael@gmail.com>
2022-09-07 23:26:03 +0100

    add textfields for title and description

--- lib/modules/todo/presentation/pages/create_todo_page.dart
+++ lib/modules/todo/presentation/pages/create_todo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_text_field.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_back_button.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_bar_avatar.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
@@ -19,12 +20,23 @@ class CreateTodoPage extends StatelessWidget {
       body: SingleChildScrollView(
         padding: SizeUtil.pSymmetric(h: 18, v: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
               'Create \nNew Todo',
               style: TextStyle(
                 fontSize: SizeUtil.fontSize(24),
                 fontWeight: FontWeight.w900,
+                height: SizeUtil.h(1.45),
+              ),
+            ),
+            SizeUtil.vS(30),
+            const AppTextField(hint: 'Title'),
+            SizeUtil.vS(20),
+            const TextField(
+              maxLines: 3,
+              decoration: InputDecoration(
+                hintText: 'Description',
               ),
             ),
           ],
