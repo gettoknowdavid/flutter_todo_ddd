95b9fb85193ce2e9d88935fe3a7dfb98104389fa
David Michael II <knowdavidmichael@gmail.com>
2022-09-07 01:49:23 +0100

    Create category.freezed.dart

---
+++ lib/modules/todo/domain/entities/category.freezed.dart
@@ -0,0 +1,194 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'category.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$Category {
+  Uid get uid => throw _privateConstructorUsedError;
+  ICategoryTitle get title => throw _privateConstructorUsedError;
+  ICategoryTodoList<dynamic> get todos => throw _privateConstructorUsedError;
+  ICategoryColor get color => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CategoryCopyWith<Category> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CategoryCopyWith<$Res> {
+  factory $CategoryCopyWith(Category value, $Res Function(Category) then) =
+      _$CategoryCopyWithImpl<$Res>;
+  $Res call(
+      {Uid uid,
+      ICategoryTitle title,
+      ICategoryTodoList<dynamic> todos,
+      ICategoryColor color});
+}
+
+/// @nodoc
+class _$CategoryCopyWithImpl<$Res> implements $CategoryCopyWith<$Res> {
+  _$CategoryCopyWithImpl(this._value, this._then);
+
+  final Category _value;
+  // ignore: unused_field
+  final $Res Function(Category) _then;
+
+  @override
+  $Res call({
+    Object? uid = freezed,
+    Object? title = freezed,
+    Object? todos = freezed,
+    Object? color = freezed,
+  }) {
+    return _then(_value.copyWith(
+      uid: uid == freezed
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as Uid,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as ICategoryTitle,
+      todos: todos == freezed
+          ? _value.todos
+          : todos // ignore: cast_nullable_to_non_nullable
+              as ICategoryTodoList<dynamic>,
+      color: color == freezed
+          ? _value.color
+          : color // ignore: cast_nullable_to_non_nullable
+              as ICategoryColor,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_CategoryCopyWith<$Res> implements $CategoryCopyWith<$Res> {
+  factory _$$_CategoryCopyWith(
+          _$_Category value, $Res Function(_$_Category) then) =
+      __$$_CategoryCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {Uid uid,
+      ICategoryTitle title,
+      ICategoryTodoList<dynamic> todos,
+      ICategoryColor color});
+}
+
+/// @nodoc
+class __$$_CategoryCopyWithImpl<$Res> extends _$CategoryCopyWithImpl<$Res>
+    implements _$$_CategoryCopyWith<$Res> {
+  __$$_CategoryCopyWithImpl(
+      _$_Category _value, $Res Function(_$_Category) _then)
+      : super(_value, (v) => _then(v as _$_Category));
+
+  @override
+  _$_Category get _value => super._value as _$_Category;
+
+  @override
+  $Res call({
+    Object? uid = freezed,
+    Object? title = freezed,
+    Object? todos = freezed,
+    Object? color = freezed,
+  }) {
+    return _then(_$_Category(
+      uid: uid == freezed
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as Uid,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as ICategoryTitle,
+      todos: todos == freezed
+          ? _value.todos
+          : todos // ignore: cast_nullable_to_non_nullable
+              as ICategoryTodoList<dynamic>,
+      color: color == freezed
+          ? _value.color
+          : color // ignore: cast_nullable_to_non_nullable
+              as ICategoryColor,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_Category implements _Category {
+  const _$_Category(
+      {required this.uid,
+      required this.title,
+      required this.todos,
+      required this.color});
+
+  @override
+  final Uid uid;
+  @override
+  final ICategoryTitle title;
+  @override
+  final ICategoryTodoList<dynamic> todos;
+  @override
+  final ICategoryColor color;
+
+  @override
+  String toString() {
+    return 'Category(uid: $uid, title: $title, todos: $todos, color: $color)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Category &&
+            const DeepCollectionEquality().equals(other.uid, uid) &&
+            const DeepCollectionEquality().equals(other.title, title) &&
+            const DeepCollectionEquality().equals(other.todos, todos) &&
+            const DeepCollectionEquality().equals(other.color, color));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(uid),
+      const DeepCollectionEquality().hash(title),
+      const DeepCollectionEquality().hash(todos),
+      const DeepCollectionEquality().hash(color));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_CategoryCopyWith<_$_Category> get copyWith =>
+      __$$_CategoryCopyWithImpl<_$_Category>(this, _$identity);
+}
+
+abstract class _Category implements Category {
+  const factory _Category(
+      {required final Uid uid,
+      required final ICategoryTitle title,
+      required final ICategoryTodoList<dynamic> todos,
+      required final ICategoryColor color}) = _$_Category;
+
+  @override
+  Uid get uid;
+  @override
+  ICategoryTitle get title;
+  @override
+  ICategoryTodoList<dynamic> get todos;
+  @override
+  ICategoryColor get color;
+  @override
+  @JsonKey(ignore: true)
+  _$$_CategoryCopyWith<_$_Category> get copyWith =>
+      throw _privateConstructorUsedError;
+}
