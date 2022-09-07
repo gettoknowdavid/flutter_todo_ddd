d06be06075f2647e5fa9ea186db5cc05cd69021e
David Michael II <knowdavidmichael@gmail.com>
2022-09-07 23:09:41 +0100

    Update app_providers.dart

--- lib/modules/app/application/app_providers.dart
+++ lib/modules/app/application/app_providers.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_controller.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_state.dart';
 import 'package:flutter_todo_ddd/modules/app/application/bottom_navigation/bottom_navigation_controller.dart';
-import 'package:flutter_todo_ddd/modules/app/application/bottom_navigation/bottom_navigation_state.dart';
 import 'package:flutter_todo_ddd/modules/auth/domain/i_auth_facade.dart';
 
 final appProvider = StateNotifierProvider<AppController, AppState>(
