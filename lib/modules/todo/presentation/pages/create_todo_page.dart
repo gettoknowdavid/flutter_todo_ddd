b126b09f0fe881b3a98d6710d941f00e8279f530
David Michael II <knowdavidmichael@gmail.com>
2022-09-07 23:22:44 +0100

    add app bar to CreateTodoPage

--- lib/modules/todo/presentation/pages/create_todo_page.dart
+++ lib/modules/todo/presentation/pages/create_todo_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_app_bar.dart';
+import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_back_button.dart';
+import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_bar_avatar.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class CreateTodoPage extends StatelessWidget {
@@ -8,13 +9,23 @@ class CreateTodoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppAppBar(),
+      appBar: AppBar(
+        leadingWidth: 0,
+        title: const AppBackButton(title: 'Go Back'),
+        actions: const [
+          AppBarAvatar(),
+        ],
+      ),
       body: SingleChildScrollView(
+        padding: SizeUtil.pSymmetric(h: 18, v: 20),
         child: Column(
           children: [
             Text(
-              'Create \n New Todo',
-              style: TextStyle(fontSize: SizeUtil.fontSize(30)),
+              'Create \nNew Todo',
+              style: TextStyle(
+                fontSize: SizeUtil.fontSize(24),
+                fontWeight: FontWeight.w900,
+              ),
             ),
           ],
         ),
