606f005e510c5c8677542e7a68af3f1ab88da5ad
David Michael II <knowdavidmichael@gmail.com>
2022-09-11 16:07:45 +0100

    refactor: AddTodoBottomSheet

--- lib/modules/app/application/bottom_navigation/bottom_navigation_controller.dart
+++ lib/modules/app/application/bottom_navigation/bottom_navigation_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/common/constants/app_icons.dart';
 import 'package:flutter_todo_ddd/modules/app/domain/bottom_navigation.dart';
@@ -7,11 +6,15 @@ import 'package:flutter_todo_ddd/modules/todo/presentation/pages/calendar_page.d
 import 'package:flutter_todo_ddd/modules/todo/presentation/pages/home_page.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/pages/settings_page.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/pages/stats_page.dart';
+import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/add_todo_bottom_sheet.dart';
+import 'package:flutter_todo_ddd/utils/size_util.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'bottom_navigation_controller.freezed.dart';
 part 'bottom_navigation_event.dart';
 part 'bottom_navigation_state.dart';
+
+
 
 class BottomNavigationController extends StateNotifier<BottomNavigationState> {
   BottomNavigationController() : super(BottomNavigationState.initial());
@@ -23,56 +26,20 @@ class BottomNavigationController extends StateNotifier<BottomNavigationState> {
       case 1:
         return const CalendarPage();
       case 2:
-        Modular.to.navigate('/createTodo');
-        // showModalBottomSheet(
-        //   context: context,
-        //   constraints: BoxConstraints(minHeight: SizeUtil.sh(0.7)),
-        //   backgroundColor: Theme.of(context).colorScheme.secondary,
-        //   shape: RoundedRectangleBorder(
-        //     borderRadius: BorderRadius.vertical(
-        //       top: Radius.circular(SizeUtil.r(26)),
-        //     ),
-        //   ),
-        //   builder: (context) {
-        //     return SingleChildScrollView(
-        //       padding: SizeUtil.pFromLTRB(18, 14, 18, 18),
-        //       child: Column(
-        //         crossAxisAlignment: CrossAxisAlignment.stretch,
-        //         children: [
-        //           Center(
-        //             child: Container(
-        //               height: SizeUtil.h(4),
-        //               width: SizeUtil.sw(0.15),
-        //               decoration: BoxDecoration(
-        //                 color: Colors.black26,
-        //                 borderRadius: SizeUtil.borderRadius(20),
-        //               ),
-        //             ),
-        //           ),
-        //           SizeUtil.vS(20),
-        //           Text(
-        //             'Add a new todo',
-        //             style: TextStyle(
-        //               fontSize: SizeUtil.fontSize(24),
-        //               fontWeight: FontWeight.bold,
-        //             ),
-        //           ),
-        //           SizeUtil.vS(10),
-        //           AppTextField(hint: 'Title'),
-        //           SizeUtil.vS(10),
-        //           TextField(
-        //             maxLines: 3,
-        //             decoration: InputDecoration(
-        //               hintText: 'Description',
-        //             ),
-        //           ),
-        //           SizeUtil.vS(20),
-        //           AppButton(onPressed: () {}, title: 'Add'),
-        //         ],
-        //       ),
-        //     );
-        //   },
-        // );
+        // Modular.to.pushNamed('/createTodo');
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          useRootNavigator: true,
+          constraints: BoxConstraints(minHeight: SizeUtil.sh(0.7)),
+          backgroundColor: Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(SizeUtil.r(26)),
+            ),
+          ),
+          builder: (context) => const AddTodoBottomSheet(),
+        );
         return state.page;
       case 3:
         return const StatsPage();
