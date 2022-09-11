edcca164e73ee61c29f41ae8438555d5bc131151
David Michael II <knowdavidmichael@gmail.com>
2022-09-11 16:12:53 +0100

    remove CreateTodoPage from route

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -16,7 +16,6 @@ import 'package:flutter_todo_ddd/modules/todo/application/todo_form/todo_form_co
 import 'package:flutter_todo_ddd/modules/todo/domain/i_todo_facade.dart';
 import 'package:flutter_todo_ddd/modules/todo/infrastructure/todo_facade.dart';
 import 'package:flutter_todo_ddd/modules/todo/infrastructure/todo_mapper.dart';
-import 'package:flutter_todo_ddd/modules/todo/presentation/pages/create_todo_page.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/pages/profile_page.dart';
 import 'package:flutter_todo_ddd/services/open_mail_app_facade.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -46,6 +45,5 @@ class AppModule extends Module {
     ChildRoute('/forgotPassword', child: (_, __) => ForgotPasswordPage()),
     ChildRoute('/layout', child: (_, __) => const AppLayout()),
     ChildRoute('/profile', child: (_, __) => const ProfilePage()),
-    ChildRoute('/createTodo', child: (_, __) => const CreateTodoPage()),
   ];
 }
