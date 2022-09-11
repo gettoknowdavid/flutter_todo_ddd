b652def3f4e86fd86b9ce3a9aa314435a03beda9
David Michael II <knowdavidmichael@gmail.com>
2022-09-11 21:48:53 +0100

    add validateTodoDescription

--- lib/modules/todo/domain/value_validators.dart
+++ lib/modules/todo/domain/value_validators.dart
@@ -9,11 +9,21 @@ Either<ValueFailure<String>, String> validateCategoryNotEmpty(String input) {
   }
 }
 
-Either<ValueFailure<String>, String> validateTitleNotEmpty(String input) {
+Either<ValueFailure<String>, String> validateTodoTitle(String input) {
   if (input.isEmpty) {
     return left(ValueFailure.empty(input));
-  } else if (input.length > 250) {
+  } else if (input.length > 150) {
     return left(ValueFailure.titleLengthExceeded(input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<String>, String?> validateTodoDescription(String? input) {
+  if (input == null) {
+    return right(input);
+  } else if (input.length > 250) {
+    return left(ValueFailure.descLengthExceeded(input));
   } else {
     return right(input);
   }
