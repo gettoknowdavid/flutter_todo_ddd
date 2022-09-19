b23d105e473965e13a461e23e22eb39507953462
David Michael II <knowdavidmichael@gmail.com>
2022-09-19 23:28:03 +0100

    refactor: CreateCategoryDropDown widget

--- lib/modules/todo/presentation/widgets/add_todo_bottom_sheet.dart
+++ lib/modules/todo/presentation/widgets/add_todo_bottom_sheet.dart
@@ -1,21 +1,13 @@
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_todo_ddd/common/constants/app_icons.dart';
 import 'package:flutter_todo_ddd/common/widgets/app_button.dart';
-import 'package:flutter_todo_ddd/common/widgets/app_icon.dart';
 import 'package:flutter_todo_ddd/common/widgets/app_text_field.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/todo_form/todo_form_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/todo_provider.dart';
+import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/add_category_dialog.dart';
+import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/add_todo_date_field.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
-import 'package:flutter_todo_ddd/utils/date_formatter.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
-
-const _items = <Cat>[
-  Cat(1, 'Home'),
-  Cat(2, 'Work'),
-  Cat(2, 'Church'),
-];
 
 class AddTodoBottomSheet extends ConsumerStatefulWidget {
   const AddTodoBottomSheet({super.key});
@@ -26,16 +18,11 @@ class AddTodoBottomSheet extends ConsumerStatefulWidget {
 
 class _AddTodoBottomSheetState extends ConsumerState<AddTodoBottomSheet> {
   final _formKey = GlobalKey<FormState>();
-  late TextEditingController timeController;
 
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(todoFormProvider);
     final event = ref.watch(todoFormProvider.notifier);
-
-    timeController = TextEditingController(
-      text: dateFormat(state.todo.time),
-    );
 
     return StatefulBuilder(builder: (context, setState) {
       return SingleChildScrollView(
@@ -46,6 +33,7 @@ class _AddTodoBottomSheetState extends ConsumerState<AddTodoBottomSheet> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const _HandleBar(),
               SizeUtil.vS(20),
@@ -66,13 +54,11 @@ class _AddTodoBottomSheetState extends ConsumerState<AddTodoBottomSheet> {
                 ),
               ),
               SizeUtil.vS(14),
-              TextFormField(
+              AppTextField(
                 enabled: !state.loading,
                 maxLines: 3,
-                decoration: const InputDecoration(
-                  hintText: 'Description',
-                  label: Text('Description'),
-                ),
+                hint: 'Description',
+                label: 'Description',
                 onChanged: (value) => event.mapEventsToStates(
                   TodoFormEvent.descChanged(value),
                 ),
@@ -86,89 +72,15 @@ class _AddTodoBottomSheetState extends ConsumerState<AddTodoBottomSheet> {
               SizeUtil.vS(14),
               Row(
                 children: [
-                  Expanded(
-                    child: DropdownButton2<String>(
-                      items: _items
-                          .map(
-                            (e) => DropdownMenuItem<String>(
-                              value: e.title,
-                              child: Text(e.title),
-                            ),
-                          )
-                          .toList(),
-                      buttonHeight: SizeUtil.h(58),
-                      buttonDecoration: BoxDecoration(
-                        borderRadius: SizeUtil.borderRadius(20),
-                        color: Theme.of(context).primaryColorLight,
-                        border: Border.all(color: Colors.transparent),
-                      ),
-                      buttonWidth: SizeUtil.sh(1),
-                      buttonPadding: SizeUtil.pOnly(l: 22, r: 12),
-                      dropdownDecoration: BoxDecoration(
-                        borderRadius: SizeUtil.borderRadius(20),
-                        color: Theme.of(context).primaryColorLight,
-                      ),
-                      hint: const Text('Select category'),
-                      itemHeight: SizeUtil.h(58),
-                      onChanged: (value) {},
-                      underline: const SizedBox(),
-                    ),
+                  const Expanded(
+                    child: CategoryDropDown(),
                   ),
                   SizeUtil.hS(10),
-                  RawMaterialButton(
-                    onPressed: () {},
-                    constraints: BoxConstraints(
-                      minWidth: SizeUtil.h(40),
-                      minHeight: SizeUtil.h(40),
-                    ),
-                    shape: CircleBorder(
-                      side: BorderSide(
-                        color: Colors.black12,
-                        width: SizeUtil.r(2),
-                      ),
-                    ),
-                    child: Icon(Icons.add, size: SizeUtil.r(20)),
-                  ),
+                  const AddCategoryButton(),
                 ],
               ),
               SizeUtil.vS(14),
-              TextFormField(
-                controller: timeController,
-                decoration: InputDecoration(
-                  hintText: 'Select date and time',
-                  suffixIcon: AppIcon(
-                    icon: AppIcons.calendar,
-                    semantic: 'Calendar',
-                    width: SizeUtil.w(20),
-                  ),
-                ),
-                onTap: () async {
-                  DateTime? date = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime(2025),
-                  );
-
-                  TimeOfDay? time = await showTimePicker(
-                    context: context,
-                    initialTime: const TimeOfDay(hour: 9, minute: 0),
-                  );
-
-                  if (date != null && time != null) {
-                    DateTime dt = DateTime(
-                      date.year,
-                      date.month,
-                      date.day,
-                      time.hour,
-                      time.minute,
-                    );
-                    event.mapEventsToStates(TodoFormEvent.timeChanged(dt));
-                    setState(() => timeController.text = dateFormat(dt));
-                  }
-                },
-                readOnly: true,
-              ),
+              const AddTodoDateField(),
               SizeUtil.vS(14),
               SwitchListTile(
                 value: state.todo.isDone,
@@ -182,9 +94,7 @@ class _AddTodoBottomSheetState extends ConsumerState<AddTodoBottomSheet> {
               AppButton(
                 disabled: !state.todo.title.isValid() ||
                     !state.todo.description!.isValid(),
-                onPressed: () {
-                  print(state.todo);
-                },
+                onPressed: () => print(state.todo),
                 loading: state.loading,
                 title: 'Add',
               ),
@@ -205,13 +115,6 @@ class _AddTodoBottomSheetState extends ConsumerState<AddTodoBottomSheet> {
     // );
     super.initState();
   }
-}
-
-class Cat {
-  final int id;
-  final String title;
-
-  const Cat(this.id, this.title);
 }
 
 class _HandleBar extends StatelessWidget {
