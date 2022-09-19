773090821fb51682e871501ccfe059c91e6e8f9d
David Michael II <knowdavidmichael@gmail.com>
2022-09-19 23:30:27 +0100

    convert to ConsumerWidget

--- lib/modules/todo/presentation/widgets/add_todo_form.dart
+++ lib/modules/todo/presentation/widgets/add_todo_form.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddTodoForm extends StatelessWidget {
+class AddTodoForm extends ConsumerWidget {
   const AddTodoForm({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container();
   }
 }
