55d8d9a8e279cde925615857d6644596115ba6a5
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 13:27:54 +0100

    add watchAll event

--- lib/modules/todo/application/category/category_event.dart
+++ lib/modules/todo/application/category/category_event.dart
@@ -3,9 +3,9 @@ part of 'category_controller.dart';
 @freezed
 class CategoryEvent with _$CategoryEvent {
   const factory CategoryEvent.getAll() = _GetAll;
-  const factory CategoryEvent.getUncompletedStarted() =
-      _GetUncompletedStarted;
+  const factory CategoryEvent.watchAll() = _WatchAll;
+  const factory CategoryEvent.getUncompletedStarted() = _GetUncompletedStarted;
   const factory CategoryEvent.categoriesReceived(
-          Either<CategoryFailure, List<Category>> failureOrNotes) =
+          Either<CategoryFailure, List<Category?>> failureOrNotes) =
       _CategoriesReceived;
 }
