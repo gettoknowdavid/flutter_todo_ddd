faea8e22a4a6396c265543be1200a33cc44ffb8a
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 01:49:10 +0100

    add category provider

--- lib/modules/todo/application/category_provider.dart
+++ lib/modules/todo/application/category_provider.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/todo/application/category/category_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/category_form/category_form_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/i_category_facade.dart';
 
-final categoryProvider = StateNotifierProvider.autoDispose<
+final categoryFormProvider = StateNotifierProvider.autoDispose<
     CategoryFormController, CategoryFormState>(
   (ref) => CategoryFormController(Modular.get<ICategoryFacade>()),
+);
+
+final categoryProvider =
+    StateNotifierProvider<CategoryController, CategoryState>(
+  (ref) => CategoryController(Modular.get<ICategoryFacade>()),
 );
