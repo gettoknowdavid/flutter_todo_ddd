f442df8731c29cec7ad28b892cdb2ef6c68444d5
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 19:49:44 +0100

    refactor: CategoryTile

--- lib/modules/todo/presentation/pages/home_page.dart
+++ lib/modules/todo/presentation/pages/home_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_bar_avatar
 import 'package:flutter_todo_ddd/modules/todo/application/category/category_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/category_provider.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/entities/category.dart';
+import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/category_tile.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/search_box.dart';
 import 'package:flutter_todo_ddd/theme/app_colors.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
@@ -63,62 +64,6 @@ class HomePage extends ConsumerWidget {
                 ),
               ),
             ]
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CategoryTile extends StatelessWidget {
-  const CategoryTile({
-    Key? key,
-    required this.category,
-    required this.index,
-  }) : super(key: key);
-
-  final Category category;
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        height: (index % 2.05 + 1.3) * 90,
-        padding: SizeUtil.pAll(18),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: category.color.getOrCrash()!,
-          borderRadius: SizeUtil.borderRadius(30),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              category.title.getOrCrash()!,
-              style: TextStyle(
-                fontSize: SizeUtil.fontSize(18),
-                color: ThemeData.estimateBrightnessForColor(
-                            category.color.getOrCrash()!) ==
-                        Brightness.light
-                    ? AppColors.black
-                    : AppColors.white,
-              ),
-            ),
-            SizeUtil.hS(10),
-            Text(
-              '4 Tasks',
-              style: TextStyle(
-                fontSize: SizeUtil.fontSize(14),
-                color: ThemeData.estimateBrightnessForColor(
-                            category.color.getOrCrash()!) ==
-                        Brightness.light
-                    ? Colors.black45
-                    : Colors.white54,
-              ),
-            ),
           ],
         ),
       ),
