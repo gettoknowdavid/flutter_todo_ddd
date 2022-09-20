6229b1c07e5f8cd50cd07a9b2cb8c33ae65258e4
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 20:53:51 +0100

    add onPressed prop and null safety for title

--- lib/modules/app/presentation/widgets/app_back_button.dart
+++ lib/modules/app/presentation/widgets/app_back_button.dart
@@ -3,16 +3,21 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
 
 class AppBackButton extends StatelessWidget {
-  final String title;
+  final String? title;
+  final void Function()? onPressed;
 
-  const AppBackButton({Key? key, required this.title}) : super(key: key);
+  const AppBackButton({
+    Key? key,
+    this.title,
+    this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton.icon(
       icon: const BackButtonIcon(),
-      label: Text(title),
-      onPressed: () => Modular.to.pop(),
+      label: title == null ? const SizedBox() : Text(title!),
+      onPressed: onPressed ?? () => Modular.to.pop(),
       style: TextButton.styleFrom(
         backgroundColor: Theme.of(context).primaryColorLight,
         padding: SizeUtil.pFromLTRB(12, 0, 12, 0),
