f33852bee86e3af364a2df2d8b4a5ceda34cf68d
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 22:08:12 +0100

    add all factory

--- lib/modules/todo/domain/entities/category.dart
+++ lib/modules/todo/domain/entities/category.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_todo_ddd/core/uid.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/value_objects.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -17,6 +18,13 @@ class Category with _$Category {
         uid: Uid(),
         title: ICategoryTitle(''),
         color: ICategoryColor(ICategoryColor.colors[0]),
+        createdAt: DateTime.now(),
+      );
+
+  factory Category.all() => Category(
+        uid: Uid(),
+        title: ICategoryTitle('All'),
+        color: ICategoryColor(Colors.black),
         createdAt: DateTime.now(),
       );
 }
