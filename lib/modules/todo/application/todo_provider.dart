5a4488946d9083512f5eef55afae00473b6ec9de
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 21:11:45 +0100

    add todoProvider

--- lib/modules/todo/application/todo_provider.dart
+++ lib/modules/todo/application/todo_provider.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_ddd/modules/todo/application/todo/todo_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/todo_form/todo_form_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/i_todo_facade.dart';
 
 final todoFormProvider =
     StateNotifierProvider.autoDispose<TodoFormController, TodoFormState>(
   (ref) => TodoFormController(Modular.get<ITodoFacade>()),
+);
+
+final todoProvider = StateNotifierProvider<TodoController, TodoState>(
+  (ref) => TodoController(Modular.get<ITodoFacade>()),
 );
