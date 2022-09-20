7131fe094b25114ce01ba5f54f16d425414524a4
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 19:48:30 +0100

    add staggered category list

--- lib/modules/todo/presentation/pages/home_page.dart
+++ lib/modules/todo/presentation/pages/home_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_todo_ddd/common/widgets/app_loading_indicator.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_providers.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_bar_avatar.dart';
+import 'package:flutter_todo_ddd/modules/todo/application/category/category_controller.dart';
+import 'package:flutter_todo_ddd/modules/todo/application/category_provider.dart';
+import 'package:flutter_todo_ddd/modules/todo/domain/entities/category.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/search_box.dart';
+import 'package:flutter_todo_ddd/theme/app_colors.dart';
 import 'package:flutter_todo_ddd/theme/app_text_styles.dart';
 import 'package:flutter_todo_ddd/utils/get_greeting.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
@@ -12,6 +18,8 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final categoryState = ref.watch(categoryProvider);
+
     return Scaffold(
       appBar: AppBar(
         actions: const [
@@ -22,11 +30,95 @@ class HomePage extends ConsumerWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizeUtil.vS(20),
+            SizeUtil.vS(10),
             const _NameHeader(),
             SizeUtil.vS(30),
             const SearchBox(),
             SizeUtil.vS(30),
+            if (categoryState is CategoryLoading) ...[
+              Center(
+                child: SizedBox(
+                  width: SizeUtil.sw(0.2),
+                  child: const AppLoadingIndicator(),
+                ),
+              ),
+            ],
+            if (categoryState is CategorySuccess) ...[
+              Container(
+                padding: SizeUtil.pSymmetric(h: 18),
+                height: SizeUtil.sh(0.55),
+                child: MasonryGridView.extent(
+                  primary: false,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: categoryState.categories.length,
+                  maxCrossAxisExtent: SizeUtil.sw(0.5),
+                  mainAxisSpacing: SizeUtil.h(18),
+                  crossAxisSpacing: SizeUtil.h(18),
+                  itemBuilder: (context, index) {
+                    return CategoryTile(
+                      category: categoryState.categories[index]!,
+                      index: index,
+                    );
+                  },
+                ),
+              ),
+            ]
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  const CategoryTile({
+    Key? key,
+    required this.category,
+    required this.index,
+  }) : super(key: key);
+
+  final Category category;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: (index % 2.05 + 1.3) * 90,
+        padding: SizeUtil.pAll(18),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: category.color.getOrCrash()!,
+          borderRadius: SizeUtil.borderRadius(30),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              category.title.getOrCrash()!,
+              style: TextStyle(
+                fontSize: SizeUtil.fontSize(18),
+                color: ThemeData.estimateBrightnessForColor(
+                            category.color.getOrCrash()!) ==
+                        Brightness.light
+                    ? AppColors.black
+                    : AppColors.white,
+              ),
+            ),
+            SizeUtil.hS(10),
+            Text(
+              '4 Tasks',
+              style: TextStyle(
+                fontSize: SizeUtil.fontSize(14),
+                color: ThemeData.estimateBrightnessForColor(
+                            category.color.getOrCrash()!) ==
+                        Brightness.light
+                    ? Colors.black45
+                    : Colors.white54,
+              ),
+            ),
           ],
         ),
       ),
