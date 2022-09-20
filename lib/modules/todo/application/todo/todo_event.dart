d8095b1806faa1a5b876805787bc1c6a61213782
David Michael II <knowdavidmichael@gmail.com>
2022-09-20 21:11:15 +0100

    create TodoEvent

--- lib/modules/todo/application/todo/todo_event.dart
+++ lib/modules/todo/application/todo/todo_event.dart
@@ -1,1 +1,10 @@
+part of 'todo_controller.dart';
 
+@freezed
+class TodoEvent with _$TodoEvent {
+  const factory TodoEvent.watchAll() = _WatchAll;
+  const factory TodoEvent.watchUncompleted() = _WatchUncompleted;
+  const factory TodoEvent.todosReceived(
+    Either<TodoFailure, List<Todo?>> either,
+  ) = _TodosReceived;
+}
