99604d4c8474bc3ce34e8502cb695dd37e02d369
David Michael II <knowdavidmichael@gmail.com>
2022-09-21 17:19:55 +0100

    add todo edit event

--- lib/modules/todo/application/todo_form/todo_form_event.dart
+++ lib/modules/todo/application/todo_form/todo_form_event.dart
@@ -3,6 +3,7 @@ part of 'todo_form_controller.dart';
 @freezed
 class TodoFormEvent with _$TodoFormEvent {
   const factory TodoFormEvent.descChanged(String desc) = _TodoDescChanged;
+  const factory TodoFormEvent.edit(Todo todo) = _TodoEdit;
   const factory TodoFormEvent.initialized(Option<Todo> initialOption) =
       _TodoFormInitialized;
   const factory TodoFormEvent.isDoneChanged(bool isDone) = _TodoIsDoneChanged;
