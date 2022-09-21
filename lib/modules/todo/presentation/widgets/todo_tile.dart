fc575faacddeb431c92020d88380d4ca8838be1c
David Michael II <knowdavidmichael@gmail.com>
2022-09-21 21:28:41 +0100

    add showModalBottomSheet to TodoTile

--- lib/modules/todo/presentation/widgets/todo_tile.dart
+++ lib/modules/todo/presentation/widgets/todo_tile.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/todo_form/todo_form_controller.dart';
 import 'package:flutter_todo_ddd/modules/todo/application/todo_provider.dart';
 import 'package:flutter_todo_ddd/modules/todo/domain/entities/todo.dart';
+import 'package:flutter_todo_ddd/modules/todo/presentation/widgets/add_todo_bottom_sheet.dart';
 import 'package:flutter_todo_ddd/theme/app_colors.dart';
 import 'package:flutter_todo_ddd/utils/date_formatter.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
@@ -19,59 +20,69 @@ class TodoTile extends ConsumerWidget {
 
     final todo = TodoDto.fromDomain(todoEntity);
 
-    return Container(
-      height: SizeUtil.h(90),
-      width: SizeUtil.sw(1),
-      margin: SizeUtil.pOnly(b: 12),
-      padding: SizeUtil.pSymmetric(h: 8, v: 18),
-      alignment: Alignment.centerLeft,
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: SizeUtil.borderRadius(26),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: SizeUtil.r(4),
-            color: Colors.black12,
-          )
-        ],
-      ),
-      child: Row(
-        children: [
-          Checkbox(
-            value: todo.isDone,
-            onChanged: (value) {
-              event.mapEventsToStates(
-                TodoFormEvent.edit(todoEntity.copyWith(isDone: value!)),
-              );
-            },
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  todo.title,
-                  style: TextStyle(
-                    color: AppColors.black,
-                    fontSize: SizeUtil.fontSize(16),
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                if (todo.time != null) ...[
-                  SizeUtil.vS(4),
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          useRootNavigator: true,
+          builder: (context) => AddTodoBottomSheet(isEdit: true, todo: todo),
+        );
+      },
+      child: Container(
+        height: SizeUtil.h(90),
+        width: SizeUtil.sw(1),
+        margin: SizeUtil.pOnly(b: 12),
+        padding: SizeUtil.pSymmetric(h: 8, v: 18),
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: SizeUtil.borderRadius(26),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: SizeUtil.r(4),
+              color: Colors.black12,
+            )
+          ],
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: todo.isDone,
+              onChanged: (value) {
+                event.mapEventsToStates(
+                  TodoFormEvent.edit(todoEntity.copyWith(isDone: value!)),
+                );
+              },
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Text(
-                    'Due ${dateFormat(todo.time)}',
+                    todo.title,
                     style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: SizeUtil.fontSize(12),
+                      color: AppColors.black,
+                      fontSize: SizeUtil.fontSize(16),
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
-                ]
-              ],
+                  if (todo.time != null) ...[
+                    SizeUtil.vS(4),
+                    Text(
+                      'Due ${dateFormat(todo.time)}',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: SizeUtil.fontSize(12),
+                      ),
+                    ),
+                  ]
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
