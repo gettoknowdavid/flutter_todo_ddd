807e0fea00e2dbe245d2d5f83240a9ce90f648c3
David Michael II <knowdavidmichael@gmail.com>
2022-09-22 16:45:38 +0100

    add new events watchUpcoming, watchToday, watchDone

--- lib/modules/todo/application/todo/todo_event.dart
+++ lib/modules/todo/application/todo/todo_event.dart
@@ -3,6 +3,9 @@ part of 'todo_controller.dart';
 @freezed
 class TodoEvent with _$TodoEvent {
   const factory TodoEvent.watchAll() = _WatchAll;
+  const factory TodoEvent.watchUpcoming() = _WatchAll;
+  const factory TodoEvent.watchToday() = _WatchAll;
+  const factory TodoEvent.watchDone() = _WatchAll;
   const factory TodoEvent.watchUncompleted() = _WatchUncompleted;
   const factory TodoEvent.todosReceived(
     Either<TodoFailure, List<Todo?>> either,
