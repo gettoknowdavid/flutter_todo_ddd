472a73acaea6c7135264d4c9838bedcee924e98b
David Michael II <knowdavidmichael@gmail.com>
2022-09-22 17:03:24 +0100

    fix: events

--- lib/modules/todo/application/todo/todo_event.dart
+++ lib/modules/todo/application/todo/todo_event.dart
@@ -2,12 +2,12 @@ part of 'todo_controller.dart';
 
 @freezed
 class TodoEvent with _$TodoEvent {
-  const factory TodoEvent.watchAll() = _WatchAll;
-  const factory TodoEvent.watchUpcoming() = _WatchAll;
-  const factory TodoEvent.watchToday() = _WatchAll;
-  const factory TodoEvent.watchDone() = _WatchAll;
-  const factory TodoEvent.watchUncompleted() = _WatchUncompleted;
   const factory TodoEvent.todosReceived(
     Either<TodoFailure, List<Todo?>> either,
   ) = _TodosReceived;
+  const factory TodoEvent.watchAll() = _WatchAll;
+  const factory TodoEvent.watchDone() = _WatchDone;
+  const factory TodoEvent.watchToday() = _WatchToday;
+  const factory TodoEvent.watchUncompleted() = _WatchUncompleted;
+  const factory TodoEvent.watchUpcoming() = _WatchUpcoming;
 }
