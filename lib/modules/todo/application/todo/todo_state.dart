b11bd9731f9331952a01de4e5a0dc092e6adb9a4
David Michael II <knowdavidmichael@gmail.com>
2022-09-22 20:23:39 +0100

    add lengths

--- lib/modules/todo/application/todo/todo_state.dart
+++ lib/modules/todo/application/todo/todo_state.dart
@@ -4,6 +4,12 @@ part of 'todo_controller.dart';
 class TodoState with _$TodoState {
   const factory TodoState.initial() = TodoInitial;
   const factory TodoState.loading() = TodoLoading;
-  const factory TodoState.loadSuccess(List<Todo?> todos) = TodoSuccess;
+  const factory TodoState.loadSuccess({
+    required List<Todo?> todos,
+    required int allTodosLength,
+    required int doneTodosLength,
+    required int todayTodosLength,
+    required int upcomingTodosLength,
+  }) = TodoSuccess;
   const factory TodoState.loadFailure(TodoFailure failure) = TodoFailed;
 }
