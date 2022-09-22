edc68822123257318d32c275de425394e8d307c7
David Michael II <knowdavidmichael@gmail.com>
2022-09-22 07:01:45 +0100

    add color and name extensions

--- lib/modules/todo/domain/entities/category.dart
+++ lib/modules/todo/domain/entities/category.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_ddd/theme/app_colors.dart';
+
 enum Category { all, today, inbox, upComing }
+
+extension CategoryColor on Category {
+  Color get color {
+    switch (this) {
+      case Category.inbox:
+        return AppColors.lavender;
+      case Category.upComing:
+        return Colors.green;
+      case Category.today:
+        return AppColors.orange;
+      default:
+        return AppColors.black;
+    }
+  }
+}
+
+extension CategoryName on Category {
+  String get name {
+    switch (this) {
+      case Category.inbox:
+        return 'Inbox';
+      case Category.upComing:
+        return 'Upcoming';
+      case Category.today:
+        return 'Today';
+      default:
+        return 'All';
+    }
+  }
+}
