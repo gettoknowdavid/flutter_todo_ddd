b85a6304d78a95fce86f6a49d773d7506861708f
David Michael II <knowdavidmichael@gmail.com>
2022-11-25 13:09:38 +0100

    add loading page

--- lib/modules/app/presentation/pages/app_layout.dart
+++ lib/modules/app/presentation/pages/app_layout.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_todo_ddd/common/widgets/app_loading_indicator.dart';
 import 'package:flutter_todo_ddd/modules/app/application/app_providers.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_app_bar.dart';
 import 'package:flutter_todo_ddd/modules/app/presentation/widgets/app_bottom_navigation_bar.dart';
@@ -9,6 +8,7 @@ import 'package:flutter_todo_ddd/modules/auth/application/auth_providers.dart';
 import 'package:flutter_todo_ddd/modules/auth/application/auth_state.dart';
 import 'package:flutter_todo_ddd/modules/todo/presentation/pages/home_page.dart';
 import 'package:flutter_todo_ddd/utils/size_util.dart';
+import 'package:lottie/lottie.dart';
 
 class AppLayout extends ConsumerWidget {
   const AppLayout({Key? key}) : super(key: key);
@@ -29,11 +29,17 @@ class AppLayout extends ConsumerWidget {
 
     if (appState.loading) {
       return Scaffold(
-        backgroundColor: theme.colorScheme.secondary,
+        backgroundColor: theme.colorScheme.primary,
         body: Center(
           child: SizedBox(
-            width: SizeUtil.sw(0.25),
-            child: const AppLoadingIndicator(),
+            width: SizeUtil.sw(0.2),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/animations/loading.json'),
+                const Text('Loading'),
+              ],
+            ),
           ),
         ),
       );
